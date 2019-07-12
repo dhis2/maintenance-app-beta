642edcf568fde56de2b1ad21d42502ed4f71c44d
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-07-12 09:39:51 +0200

    refactor: use Tabs components for main navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { push } from 'connected-react-router'
+import { ScrollBar, TabBar, Tab } from '@dhis2/ui-core'
 import React, { useCallback } from 'react'
 
 import cx from 'classnames'
@@ -7,30 +9,65 @@ import cx from 'classnames'
 import { mainSectionOrder } from '../constants/sectionOrder'
 import styles from './Navigation.module.css'
 
-const Navigation = ({ disabled }) => {
-    const onClick = useCallback(
+const useOnClick = (disabled, push, to) =>
+    useCallback(
         e => {
-            disabled && e.preventDefault()
+            disabled ? e.preventDefault() : push(to)
         },
-        [disabled]
+        [disabled, to, push]
     )
 
+const isNavLinkActive = (path, currentPath) => {
+    if (path === currentPath) return true
+
+    const section = currentPath
+        .replace(/^\//, '') // remove possible leading slash
+        .split('/')[1] // field 1 contains the section, field 0 is the mode (edit / list)
+
+    return path.match(section)
+}
+
+const NavigationLink = connect(
+    state =>
+        console.log(state.router) || {
+            pathname: state.router.location.pathname,
+        },
+    dispatch => ({ push: path => dispatch(push(path)) })
+)(({ pathname, to, disabled, label, push }) => {
+    const onClick = useOnClick(disabled, push, to)
+
+    return (
+        <Tab selected={isNavLinkActive(to, pathname)} onClick={onClick}>
+            {label}
+        </Tab>
+    )
+})
+
+const Navigation = ({ disabled }) => {
     return (
         <nav
             className={cx({
                 [styles.disabled]: disabled,
             })}
         >
-            <Link onClick={onClick} to="/">
-                All
-            </Link>
+            <ScrollBar>
+                <TabBar>
+                    <NavigationLink
+                        to="/list/all"
+                        disabled={disabled}
+                        label={'All'}
+                    />
 
-            {mainSectionOrder.map(({ name, path }) => (
-                <Link key={path} onClick={onClick} to={path}>
-                    {' | '}
-                    {name}
-                </Link>
-            ))}
+                    {mainSectionOrder.map(({ name, path }) => (
+                        <NavigationLink
+                            key={path}
+                            to={path}
+                            label={name}
+                            disabled={disabled}
+                        />
+                    ))}
+                </TabBar>
+            </ScrollBar>
         </nav>
     )
 }
