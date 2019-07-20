f5afb4cf8278f5e5c5f0f4cf6a524dc810d564eb
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-07-20 10:00:21 +0200

    fix: shrink app height to 100vh when content shorter

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import { DataProvider } from '@dhis2/app-runtime'
 import { HeaderBar } from '@dhis2/ui-widgets'
 import React from 'react'
 import { Maintenance } from './Maintenance.app.js'
+import styles from './App.module.css'
 import './App.css'
 
 const App = () => (
     <DataProvider baseUrl={process.env.REACT_APP_DHIS2_BASE_URL} apiVersion="">
-        <div className="wrapper">
-            <HeaderBar appName="Maintenance app" />
-            <Maintenance />
+        <div className={styles.wrapper}>
+            <div className={styles.headerBar}>
+                <HeaderBar appName="Maintenance app" />
+            </div>
+            <div className={styles.appWrapper}>
+                <Maintenance />
+            </div>
         </div>
         <CssReset />
     </DataProvider>
