cd15aa78445b3bbab19bf1eb199406831fdd1e0a
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-09-03 16:05:59 +0200

    refactor: delete old layout components

--- src/components/GridSidebar.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import styles from './GridSidebar/styles.module.css'
-
-export const GridSidebar = ({ children }) => (
-    <section className={styles.sidebar}>{children}</section>
-)
