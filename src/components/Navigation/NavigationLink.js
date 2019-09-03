8feb254571dab38f2730bf83187ab96227fb5079
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-09-03 16:11:07 +0200

    refactor(navigation): simplify component code

--- src/components/Navigation/NavigationLink.js
+++ src/components/Navigation/NavigationLink.js
@@ -1,12 +1,11 @@
 import { Tab } from '@dhis2/ui-core'
-import { concat, flatten, identity, map, pipe, reduce } from 'lodash/fp'
+import { concat, filter, flatten, identity, map, pipe, reduce } from 'lodash/fp'
 import { connect } from 'react-redux'
 import { push } from 'connected-react-router'
 import { useDataQuery } from '@dhis2/app-runtime'
+import { withRouter } from 'react-router'
 import React, { useCallback } from 'react'
 import i18n from '@dhis2/d2-i18n'
-
-import { withRouter } from 'react-router'
 
 import { getAuthoritiesFromSchema } from '../../utils/authority/getAuthoritiesFromSchema'
 import { hasAuthority } from '../../utils/authority/hasAuthority'
@@ -15,6 +14,8 @@ import { queries } from '../../constants/queries'
 const AUTHORITY_NOT_DETERMINED = -1
 const HAS_NO_AUTHORITY = 0
 const HAS_AUTHORITY = 1
+
+const uncappedReduce = reduce.convert({ cap: false })
 
 const useOnClick = (disabled, push, to) =>
     useCallback(
@@ -38,28 +39,32 @@ const extractStaticPermissions = reduce(
 )
 
 /**
+ * @param {string} schemaName
+ * @returns {string}
+ */
+const createSchemaResourceUrl = schemaName =>
+    `schemas/${schemaName}.json?fields=authorities`
+
+/**
  * lodash map will convert objects to arrays
  * @param {Object[]} sections
  * @returns Object
  */
-const createSchemasQuery = sections =>
-    map(
-        section =>
-            section.schemaName
-                ? {
-                      resource: `schemas/${section.schemaName}.json?fields=authorities`,
-                  }
-                : undefined,
-        sections
+const createSchemasQuery = pipe(
+    map(section =>
+        section.schemaName
+            ? { resource: createSchemaResourceUrl(section.schemaName) }
+            : undefined
+    ),
+    filter(identity),
+    uncappedReduce(
+        (schemas, resourceQuery, index) => ({
+            ...schemas,
+            [`schema${index}`]: resourceQuery,
+        }),
+        {}
     )
-        .filter(identity)
-        .reduce(
-            (schemas, resourceQuery, index) => ({
-                ...schemas,
-                [`schema${index}`]: resourceQuery,
-            }),
-            {}
-        )
+)
 
 /**
  * @param {string[][]}
@@ -72,87 +77,62 @@ const schemasToAuthorities = staticPermissions =>
         concat(staticPermissions)
     )
 
+const getAuthorityNotDetermined = () => ({
+    loading: true,
+    error: '',
+    hasAuthority: AUTHORITY_NOT_DETERMINED,
+})
+
+const getHasNoAuthorityConfig = (error = '') => ({
+    loading: false,
+    error,
+    hasAuthority: HAS_NO_AUTHORITY,
+})
+
+const getHasAuthorityConfig = () => ({
+    loading: false,
+    error: '',
+    hasAuthority: HAS_AUTHORITY,
+})
+
+const defaultQuery = {
+    userAuthorities: queries.authorities,
+    systemSettings: queries.systemSettings,
+}
+
+/**
+ * @param {bool} noAuth
+ * @param {Object} group
+ * @returns {Object}
+ */
 const useHasAuthority = (noAuth, group) => {
-    const authoritiesQuery = noAuth
-        ? {}
-        : { userAuthorities: queries.authorities }
-    const systemSettingsQuery = noAuth
-        ? {}
-        : { systemSettings: queries.systemSettings }
-    const schemasQuery = noAuth ? {} : createSchemasQuery(group.sections)
-
-    const {
-        loading: authoritiesLoading,
-        error: authoritiesError,
-        data: { userAuthorities } = {},
-    } = useDataQuery(authoritiesQuery)
-
-    const {
-        loading: systemSettingsLoading,
-        error: systemSettingsError,
-        data: { systemSettings } = {},
-    } = useDataQuery(systemSettingsQuery)
-
-    const {
-        loading: schemasLoading,
-        error: schemasError,
-        data: schemas,
-    } = useDataQuery(schemasQuery)
-
-    if (!noAuth) {
-        if (authoritiesLoading || systemSettingsLoading || schemasLoading) {
-            return {
-                loading: true,
-                error: '',
-                hasAuthority: AUTHORITY_NOT_DETERMINED,
-            }
-        }
-
-        if (authoritiesError) {
-            return {
-                loading: false,
-                error: authoritiesError,
-                hasAuthority: AUTHORITY_NOT_DETERMINED,
-            }
-        }
-
-        if (systemSettingsError) {
-            return {
-                loading: false,
-                error: systemSettingsError,
-                hasAuthority: AUTHORITY_NOT_DETERMINED,
-            }
-        }
-
-        if (schemasError) {
-            return {
-                loading: false,
-                error: schemasError,
-                hasAuthority: AUTHORITY_NOT_DETERMINED,
-            }
-        }
+    const query = {
+        ...(noAuth ? {} : defaultQuery),
+        ...(noAuth ? {} : createSchemasQuery(group.sections)),
     }
 
-    if (!noAuth && systemSettings.keyRequireAddToView) {
+    const {
+        loading,
+        error,
+        data: { userAuthorities, systemSettings, ...schemas } = {},
+    } = useDataQuery(query)
+
+    if (noAuth) return getHasAuthorityConfig()
+    if (loading) return getAuthorityNotDetermined()
+    if (error) return getHasNoAuthorityConfig(error)
+
+    if (systemSettings.keyRequireAddToView) {
         const staticPermissions = extractStaticPermissions(group.sections)
         const requiredAuthorities = schemasToAuthorities(staticPermissions)(
             schemas
         )
 
         if (!hasAuthority(requiredAuthorities, userAuthorities)) {
-            return {
-                loading: false,
-                error: '',
-                hasAuthority: HAS_NO_AUTHORITY,
-            }
+            return getHasNoAuthorityConfig()
         }
     }
 
-    return {
-        loading: false,
-        error: '',
-        hasAuthority: HAS_AUTHORITY,
-    }
+    return getHasAuthorityConfig()
 }
 
 const NavigationLinkComponent = ({
@@ -172,12 +152,7 @@ const NavigationLinkComponent = ({
         return <Tab>{i18n.t('Checking %s', group.name)}</Tab>
     }
 
-    if (error) {
-        console.error(error)
-        return null
-    }
-
-    if (!hasAuthority) {
+    if (error || !hasAuthority) {
         return null
     }
 
--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -9,54 +9,37 @@ import { NoAuthority } from '../pages/NoAuthority'
 import { hasUserAuthorityForSection } from '../utils/authority/hasUserAuthorityForSection'
 import { queries } from '../constants/queries'
 
-const determineComponent = ({
-    loading,
-    error,
-    data,
-    permissions,
-    component,
-}) => {
-    if (loading) return Loading
-
-    if (error) return () => <Error error={error} />
-
-    if (
-        hasUserAuthorityForSection({
-            authorities: data.authorities,
-            systemSettings: data.systemSettings,
-            schema: data.schema,
-            permissions,
-        })
-    ) {
-        return component
-    }
-
-    return NoAuthority
+const defaultQuery = {
+    authorities: queries.authorities,
+    systemSettings: queries.systemSettings,
 }
 
-export const ProtectedRoute = props => {
-    const query = {
-        authorities: queries.authorities,
-        systemSettings: queries.systemSettings,
-    }
-
-    if (props.schemaName) {
-        query.schema = { resource: `schemas/${props.schemaName}.json` }
-    }
+export const ProtectedRoute = ({ permissions, schemaName, ...props }) => {
+    const query = schemaName
+        ? {
+              ...defaultQuery,
+              schema: { resource: `schemas/${schemaName}.json` },
+          }
+        : defaultQuery
 
     const { loading, error, data } = useDataQuery(query)
 
-    return (
-        <Route
-            {...props}
-            component={determineComponent({
-                ...props,
-                loading,
-                error,
-                data,
-            })}
-        />
-    )
+    const userHasAuthorityForSection = hasUserAuthorityForSection({
+        permissions,
+        authorities: data.authorities,
+        systemSettings: data.systemSettings,
+        schema: data.schema,
+    })
+
+    const component = loading
+        ? Loading
+        : error
+        ? Error
+        : userHasAuthorityForSection
+        ? props.component
+        : NoAuthority
+
+    return <Route {...props} component={component} />
 }
 
 ProtectedRoute.propTypes = {
