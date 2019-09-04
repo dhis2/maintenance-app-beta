045fa2c5e9749c0a68ab3e3154ba91f5e9e55d9f
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-09-04 09:14:36 +0200

    refactor: complete sectionsPropType & add permissionsPropType

--- src/components/Sidebar/sectionPropType.js
+++ src/components/Sidebar/sectionPropType.js
@@ -1,9 +1,13 @@
 import propTypes from 'prop-types'
 
+export const permissionPropType = propTypes.arrayOf(
+    propTypes.arrayOf(propTypes.string)
+)
+
 export const sectionPropType = propTypes.shape({
     name: propTypes.string.isRequired,
     path: propTypes.string.isRequired,
-
+    description: propTypes.string.isRequired,
     schemaName: propTypes.string,
-    permissions: propTypes.arrayOf(propTypes.arrayOf(propTypes.string)),
+    permissions: permissionPropType,
 })
