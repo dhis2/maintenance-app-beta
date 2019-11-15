32b486f3f84816068a51016679930bc566ede987
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-11-15 19:28:46 +0800

    feat: check user permissions on All and Overview pages

--- src/modules/Navigation/NavigationLink.js
+++ src/modules/Navigation/NavigationLink.js
@@ -1,14 +1,12 @@
 import { Tab } from '@dhis2/ui-core'
-import { identity } from 'lodash/fp'
 import { useSelector } from 'react-redux'
 import { withRouter } from 'react-router'
 import React, { useCallback } from 'react'
 
-import { getAuthoritiesFromSchema } from '../../utils/authority/getAuthoritiesFromSchema'
 import { getSchemasData } from '../../redux/schemas'
 import { getSystemSettingsData } from '../../redux/systemSettings'
 import { getUserAuthoritiesData } from '../../redux/userAuthority'
-import { checkAuthorities } from '../../utils/authority/checkAuthorities'
+import { hasUserAuthorityForGroup } from '../../utils/authority/hasUserAuthorityForGroup'
 
 const useOnClick = (disabled, goToPath, to) =>
     useCallback(
@@ -17,40 +15,6 @@ const useOnClick = (disabled, goToPath, to) =>
         },
         [disabled, to, goToPath]
     )
-
-/**
- * @param {bool} noAuth
- * @param {Group} group
- * @returns {AuthorityConfig}
- */
-const checkUserHasAuthorities = ({
-    noAuth,
-    group,
-    schemas,
-    systemSettings,
-    userAuthorities,
-}) => {
-    if (noAuth || !systemSettings.keyRequireAddToView) return true
-
-    const groupAuthorities = Object.entries(group.sections)
-        .map(([key, { permissions, schemaName }]) => {
-            // Static permissions in config files
-            if (permissions) return permissions
-
-            // If there are no static permissions extract them from the schemas
-            if (schemas[schemaName])
-                return getAuthoritiesFromSchema(schemas[schemaName])
-
-            // just in case there's no schema defined, should never happen theoretically
-            return null
-        })
-        .filter(identity)
-
-    // User has authority for group if he has authority for any section inside
-    return groupAuthorities.some(sectionAuthorities =>
-        checkAuthorities(sectionAuthorities, userAuthorities)
-    )
-}
 
 const NavigationLinkComponent = ({
     id,
@@ -67,7 +31,7 @@ const NavigationLinkComponent = ({
     const schemas = useSelector(getSchemasData)
     const userAuthorities = useSelector(getUserAuthoritiesData)
     const systemSettings = useSelector(getSystemSettingsData)
-    const hasAuthority = checkUserHasAuthorities({
+    const hasAuthority = hasUserAuthorityForGroup({
         noAuth,
         group,
         schemas,
