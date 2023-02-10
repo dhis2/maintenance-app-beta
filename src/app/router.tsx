ae25a07fd79dc397a4cecf97ebb297cf9a1dee33
Birk Johansson <birk@dhis2.org>
2023-02-10 17:46:15 +0100

    fix(router): add default error route

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -4,11 +4,12 @@ import {
     Outlet,
     useRouteError,
     Navigate,
+    isRouteErrorResponse,
 } from "react-router-dom";
 import { Layout } from "./layout";
 import { Sidebar } from "./sidebar";
 import React, { Suspense, lazy } from "react";
-import { CircularLoader } from "@dhis2/ui";
+import { CircularLoader, NoticeBox } from "@dhis2/ui";
 import { routes } from "../pages/";
 
 const DefaultLazyLoad = ({ element }) => (
@@ -19,14 +20,37 @@ const DefaultLazyLoad = ({ element }) => (
 const AllOverview = lazy(() => import("../pages/overview/all-overview"));
 //const DataElements = lazy(() => import("../pages/data-elements/data-elements"));
 
+const LayoutElement = () => (
+    <Layout sidebar={<Sidebar />}>
+        <Outlet />
+    </Layout>
+);
+
+const DefaultErrorRoute = () => {
+    const error = useRouteError();
+    const isRouteError = isRouteErrorResponse(error);
+
+    let title = "An error occurred";
+    let message = error?.message ?? "An unknown error occurred.";
+    if (isRouteError) {
+        title = error.statusText
+        message = error?.error?.message
+    }
+    return (
+        <Layout sidebar={<Sidebar />}>
+            <NoticeBox
+                warning={isRouteError}
+                error={!isRouteError}
+                title={title}
+            >{message}</NoticeBox>
+        </Layout>
+    );
+};
 const router = createHashRouter([
     {
         // no path means its a layout route
-        element: (
-            <Layout sidebar={<Sidebar />}>
-                <Outlet />
-            </Layout>
-        ),
+        element: <LayoutElement />,
+        errorElement: <DefaultErrorRoute />,
         // TODO: Should we list all routes here, or import children-routes from pages?
         // PROS: All routes are in one place
         // children: [
---
+++ src/pages/overview/dataElements.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const DataElementsOverview = () => {
+    throw new Error('Route not implemented')
+    return <div>Data Elements Overview</div>
+}
+
+export default DataElementsOverview
