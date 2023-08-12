d6bfbb035ee44f16c6dd99be00a081e9a9c4bc7f
Birk Johansson <birk@dhis2.org>
2023-08-12 15:30:19 +0200

    fix: dont assume i18n.language is defined

--- src/lib/date/locale.ts
+++ src/lib/date/locale.ts
@@ -1,14 +1,14 @@
 import i18n from '@dhis2/d2-i18n'
 
 // our locales use _ instead of - for locales
-const language = i18n.language.replace('_', '-')
+const language = i18n.language?.replace('_', '-') || 'en'
 
 export const selectedLocale = Intl.DateTimeFormat.supportedLocalesOf([
     language,
     'en',
 ])
 
-// not sure if there can be discrepancies between support locales
+// not sure if there can be discrepancies between supported locales
 // between DateTimeFormat and RelativeTimeFormat, but do this for safety
 export const relativeTimeLocale = Intl.RelativeTimeFormat.supportedLocalesOf([
     language,
