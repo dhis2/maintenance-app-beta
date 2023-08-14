4e69ccc03811b6f5e5e9facc21d1abf67b400779
Birk Johansson <birk@dhis2.org>
2023-08-14 19:00:49 +0200

    fix: update types

--- src/types/utility.ts
+++ src/types/utility.ts
@@ -3,7 +3,7 @@ import * as Utility from './generated/utility'
 //export type * from './generated/utility'
 import { Query as DataQuery } from './query'
 
-type Resource = {
+type Resources = {
     dataElements: Models.DataElement
     categoryCombos: Models.CategoryCombo
 }
@@ -95,25 +95,100 @@ type RecursiveModelFromFields<
         : unknown // not a key of Model, return unknown
 }
 
-// Gets the union of query.params.fields
-// note that this assumes all keys, in the case of no field-filter
-///which would not match the api - which would have some defaults
-type FieldsFromQuery<Q extends ResourceQuery> =
-    Q['resource'] extends keyof Resource
-        ? 'fields' extends keyof Q['params']
-            ? Q['params']['fields'] extends ParamFields
-                ? Q['params']['fields'][number]
-                : keyof Resource[Q['resource']] & string // if fields are not an array, return all from Model
-            : keyof Resource[Q['resource']] & string // if no fields, return all from Model
-        : string
+// only used to be able to infer the Key to prevent having to write Q['resource']
+type KeyofResources<R extends keyof Resources> = R
+
+type ModelKeysForResource<R extends keyof Resources> = keyof Resources[R] &
+    string
+
+// https://github.com/microsoft/TypeScript/issues/23182#issuecomment-379091887
+// for why we need [brackets] around types
+type NeverToString<T> = [T] extends [never] ? string : T
+
+// filter out invalid keys (eg. not known keys of the Model)
+// If MapNestedToRoot is true, also convert nested fields to the root-key (eg. categoryCombo[id,name] becomes categoryCombo)
+// This is used to check if the root keys are valid
+// When false, nested fields are not converted, but they're checked if they're valid
+// Thus, this type can be used both to get the valid root-keys (for checking if the root-keys are valid),
+// and to get the filtered valid-fields, where we need to preserve the nested fields.
+// GetValidFields<'categoryCombo[id,name]' | 'id', 'dataElements', true> becomes: 'categoryCombo' | 'id'
+// GetValidFields<'categoryCombo[id,name]' | 'id', 'dataElements', false> becomes: 'categoryCombo[id,name]' | 'id'
+type GetValidFields<
+    Fields extends string,
+    ResourceKey extends keyof Resources,
+    MapNestedToRoot extends boolean = false // if true,
+> = NeverToString<
+    RemoveSpaces<
+        keyof {
+            [Field in Fields as Field extends `${infer Nested}[${infer _}]`
+                ? Nested extends ModelKeysForResource<ResourceKey>
+                    ? MapNestedToRoot extends true
+                        ? Nested
+                        : Field
+                    : never
+                : Field extends ModelKeysForResource<ResourceKey>
+                ? Field
+                : never]: Field
+        }
+    >
+>
+
+// helper to get original string[] from query.params.fields
+type GetFieldsInQuery<Q extends ResourceQuery> =
+    Q['resource'] extends KeyofResources<infer R>
+        ? Q['params'] extends {
+              fields: ParamFields
+          }
+            ? Q['params']['fields'] //GetValidFields<F, R>
+            : never // string[]
+        : never
+
+// used to return partial model if fields cannot be inferred (eg. dynamic fields)
+// GetFieldsInQuery will be never if no fields are specified, so we return full model if thats the case
+type MaybePartial<T, Condition> = [Condition] extends [never] ? T : Partial<T>
 
 export type ModelFromResourceQuery<Q extends ResourceQuery> =
-    Q['resource'] extends keyof Resource
-        ? RecursiveModelFromFields<Resource[Q['resource']], FieldsFromQuery<Q>>
-        : unknown
+    Q['resource'] extends KeyofResources<infer ResourceKey>
+        ? GetValidFields<
+              GetFieldsInQuery<Q>[number],
+              ResourceKey,
+              true
+          > extends ModelKeysForResource<ResourceKey> // fields are valid and can be inferred, filter with those keys
+            ? RecursiveModelFromFields<
+                  Resources[ResourceKey],
+                  GetValidFields<GetFieldsInQuery<Q>[number], ResourceKey>
+              >
+            : // if fields cannot be inferred, return partial model with all fields
+              // if fields does not exist in query, return Full model as is
+              MaybePartial<
+                  RecursiveModelFromFields<
+                      Resources[ResourceKey],
+                      ModelKeysForResource<ResourceKey>
+                  >,
+                  GetFieldsInQuery<Q>
+              >
+        : unknown // not a valid resource - return unknown
 
 export type ModelsFromQuery<Q extends Query> = {
     [K in keyof Q]: Q[K]['resource'] extends keyof Resources
         ? ModelFromResourceQuery<Q[K]>
         : unknown
 }
+
+const dynamicFields = ['id', 'name']
+const resourceQuery = {
+    resource: 'dataElements',
+    params: {
+        fields: [
+            'id',
+            'name',
+            'categoryCombo[id,attributeValues[attribute[attributeValues[attribute]]]]',
+        ],
+        // fields: dynamicFields,
+        //   fields: ['id', 'birk', 'categoryCombo[id,name]'],
+    },
+} as const
+
+type RQ = ModelFromResourceQuery<typeof resourceQuery>
+const m = {} as RQ
+//rq.categoryCombo.attributeValues.map(av => av.attribute.attributeValues.map(av => av.) )
