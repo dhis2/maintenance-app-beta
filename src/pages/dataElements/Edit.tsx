6637032fee4cb30d5fa5f96bfbaa6798705fb62a
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2023-08-24 10:20:05 +0200

    feat(data element edit view): load initial values from api

--- src/pages/dataElements/Edit.tsx
+++ src/pages/dataElements/Edit.tsx
@@ -1,21 +1,40 @@
+import { useDataQuery } from '@dhis2/app-runtime'
 import { Button, ButtonStrip } from '@dhis2/ui'
 import React from 'react'
 import { Form } from 'react-final-form'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
+import { DataElement } from '../../types/generated'
 import classes from './Edit.module.css'
-import { DataElementForm } from './form'
+import { DataElementForm, useCustomAttributesQuery } from './form'
+
+type DataElementQueryResponse = {
+    dataElement: DataElement
+}
+
+function useDataElementQuery(id: string) {
+    const DATA_ELEMENT_QUERY = {
+        dataElement: {
+            resource: `dataElements/${id}`,
+            params: {
+                fields: ['*'],
+            },
+        },
+    }
+
+    return useDataQuery<DataElementQueryResponse>(DATA_ELEMENT_QUERY, {
+        variables: { id },
+    })
+}
 
 // @TODO(DataElements/edit): I suppose we want some of the initial values to be
 //   dynamic? In that case, we'd have to load them and add loading/error UIs.
-const INITIAL_VALUES = {
-    legends: [],
-    domain: 'aggregate',
-    valueType: 'number',
-    aggregationType: 'sum',
-}
-
 export const Component = () => {
     const navigate = useNavigate()
+    const params = useParams()
+
+    const dataElementQuery = useDataElementQuery(params.id as string)
+    const customAttributesQuery = useCustomAttributesQuery()
+
     const onSubmit = (values: object) => {
         console.log(
             '@TODO(DataElements/edit): Implement onSubmit; values:',
@@ -23,11 +42,56 @@ export const Component = () => {
         )
     }
 
+    const loading = dataElementQuery.loading || customAttributesQuery.loading
+    const error = dataElementQuery.error || customAttributesQuery.error
+
+    if (error && !loading) {
+        // @TODO(Edit): Implement error screen
+        return `Error: ${error.toString()}`
+    }
+
+    if (loading) {
+        // @TODO(Edit): Implement loading screen
+        return 'Loading...'
+    }
+
+    const dataElement = dataElementQuery.data?.dataElement as DataElement
+    const customAttributes = customAttributesQuery.data
+    const customAttributeInitialValues = customAttributes.reduce(
+        (acc, customAttribute) => {
+            const attributeValue = dataElement.attributeValues.find(
+                (currentAttribute) =>
+                    customAttribute.id === currentAttribute.attribute.id
+            )
+
+            return {
+                ...acc,
+                [customAttribute.id]: attributeValue?.value || '',
+            }
+        },
+        {}
+    )
+
+    const initialValues = {
+        name: dataElement.name,
+        shortName: dataElement.shortName,
+        code: dataElement.code,
+        description: dataElement.description,
+        url: dataElement.url,
+        fieldMask: dataElement.fieldMask,
+        formName: dataElement.formName,
+        valueType: dataElement.valueType,
+        aggregationType: dataElement.aggregationType,
+        categoryCombo: '', // dataElement.categoryCombo?.id,
+        optionSet: '', // dataElement.optionSet,
+        commentOptionSet: '', // dataElement.commentOptionSet,
+        legendSet: [], // dataElement.legendSet || [],
+        aggregationLevels: dataElement.aggregationLevels,
+        attributeValues: customAttributeInitialValues,
+    }
+
     return (
-        <Form
-            onSubmit={onSubmit}
-            initialValues={INITIAL_VALUES}
-        >
+        <Form onSubmit={onSubmit} initialValues={initialValues}>
             {({ handleSubmit }) => (
                 <form onSubmit={handleSubmit}>
                     <div className={classes.form}>
@@ -42,7 +106,9 @@ export const Component = () => {
 
                             <Button
                                 onClick={() => {
-                                    alert('@TODO(Data elements/edit): Implement me!')
+                                    alert(
+                                        '@TODO(Data elements/edit): Implement me!'
+                                    )
                                     navigate(-1)
                                 }}
                             >
