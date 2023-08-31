b665969a495dff9252f85329af162601cddf87ee
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2023-08-31 16:44:19 +0200

    refactor(cc use options query): use "useDataQuery" instead of "useModalGist"

--- src/components/metadataSelects/CategoryComboSelect/useOptionsQuery.ts
+++ src/components/metadataSelects/CategoryComboSelect/useOptionsQuery.ts
@@ -1,7 +1,37 @@
+import { useDataQuery } from '@dhis2/app-runtime'
+import i18n from '@dhis2/d2-i18n'
 import { useEffect, useMemo, useRef, useState } from 'react'
-import { useModelGist } from '../../../lib'
 import { SelectOption } from '../../../types'
-import { CategoryComboQueryResult } from './types'
+import { CategoryCombo, Pager } from '../../../types/generated'
+
+type CategoryComboQueryResult = {
+    categoryCombos: {
+        pager: Pager
+        categoryCombos: CategoryCombo[]
+    }
+}
+
+const CATEGORY_COMBOS_QUERY = {
+    categoryCombos: {
+        resource: 'categoryCombos',
+        params: (variables: Record<string, string>) => {
+            const params = {
+                page: variables.page,
+                pageSize: 10,
+                fields: ['id', 'displayName', 'isDefault'],
+            }
+
+            if (variables.filter) {
+                return {
+                    ...params,
+                    filter: variables.filter,
+                }
+            }
+
+            return params
+        },
+    },
+}
 
 export function useOptionsQuery({
     initialSelected,
@@ -14,10 +44,15 @@ export function useOptionsQuery({
 }) {
     const fetchedInitialOptionRef = useRef(false)
     const [loadedOptions, setLoadedOptions] = useState<SelectOption[]>([])
-    const queryResult = useModelGist<CategoryComboQueryResult>(
-        'categoryCombos/gist',
-        {},
-        { variables: { pageSize: 10, filter: '' } }
+    // The gist doesn't include the `isDefault` value, need to use `useDataQuery`
+    const queryResult = useDataQuery<CategoryComboQueryResult>(
+        CATEGORY_COMBOS_QUERY,
+        {
+            variables: {
+                page: 1,
+                filter: '',
+            },
+        }
     )
     const { data } = queryResult
 
@@ -27,14 +62,15 @@ export function useOptionsQuery({
         if (data && initialSelected && !fetchedInitialOptionRef.current) {
             fetchedInitialOptionRef.current = true
 
-            const initiallySelectedOption = data.result.find(
-                (option) => option.id === initialSelected
-            )
+            const initiallySelectedOption =
+                data.categoryCombos.categoryCombos.find(
+                    (option) => option.id === initialSelected
+                )
 
             if (initiallySelectedOption) {
                 setSelectedOption({
                     value: initialSelected as string,
-                    label: initiallySelectedOption.name,
+                    label: initiallySelectedOption.displayName,
                 })
             } else if (initialSelected) {
                 fetchInitialOption()
@@ -45,17 +81,20 @@ export function useOptionsQuery({
     // Must be done in `useEffect` and not in `onComplete`, as `onComplete`
     // won't get called when useDataQuery has the values in cache already
     useEffect(() => {
-        if (data) {
-            const { pager, result } = data
+        if (data?.categoryCombos) {
+            const { pager, categoryCombos } = data.categoryCombos
             // We want to add new options to existing ones so we don't have to
             // refetch existing options
             setLoadedOptions((prevLoadedOptions) => [
                 // We only want to add when the current page is > 1
                 ...(pager.page === 1 ? [] : prevLoadedOptions),
-                ...(result.map(({ id, name }) => ({
-                    value: id,
-                    label: name,
-                })) || []),
+                ...(categoryCombos.map((catCombo) => {
+                    const { id, displayName, isDefault } = catCombo
+                    return {
+                        value: id,
+                        label: isDefault ? i18n.t('None') : displayName,
+                    }
+                }) || []),
             ])
         }
     }, [data])
@@ -64,7 +103,7 @@ export function useOptionsQuery({
         () => ({
             ...queryResult,
             data: {
-                pager: queryResult.data?.pager,
+                pager: queryResult.data?.categoryCombos.pager,
                 result: loadedOptions,
             },
         }),
