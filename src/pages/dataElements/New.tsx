32f7e20a1ae4f7a20d5971c7546ba3de42b38aa7
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2023-10-04 17:15:56 +0200

    feat(de edit and new): handle loading and error states correctly

--- src/pages/dataElements/New.tsx
+++ src/pages/dataElements/New.tsx
@@ -5,7 +5,11 @@ import { FORM_ERROR } from 'final-form'
 import React, { useEffect, useRef } from 'react'
 import { Form } from 'react-final-form'
 import { useNavigate } from 'react-router-dom'
-import { StandardFormActions, StandardFormSection } from '../../components'
+import {
+    Loader,
+    StandardFormActions,
+    StandardFormSection,
+} from '../../components'
 import { SCHEMA_SECTIONS, getSectionPath } from '../../lib'
 import { Attribute } from '../../types/generated'
 import { DataElementFormFields, useCustomAttributesQuery } from './form'
@@ -80,27 +84,11 @@ function formatFormValues({ values }: { values: FormValues }) {
     }
 }
 
-// @TODO(DataElements/new): values dynamic or static?
 export const Component = () => {
     const dataEngine = useDataEngine()
-
     const navigate = useNavigate()
     const customAttributesQuery = useCustomAttributesQuery()
-
-    const loading = customAttributesQuery.loading
-    const error = customAttributesQuery.error
-
-    if (error && !loading) {
-        // @TODO(Edit): Implement error screen
-        return <>Error: {error.toString()}</>
-    }
-
-    if (loading) {
-        // @TODO(Edit): Implement loading screen
-        return <>Loading...</>
-    }
-
-    const initialValues = computeInitialValues(customAttributesQuery.data)
+    const initialValues = computeInitialValues(customAttributesQuery.data || [])
 
     async function onSubmit(values: FormValues) {
         const payload = formatFormValues({ values })
@@ -120,16 +108,21 @@ export const Component = () => {
     }
 
     return (
-        <Form onSubmit={onSubmit} initialValues={initialValues}>
-            {({ handleSubmit, submitting, submitError }) => (
-                <form onSubmit={handleSubmit}>
-                    <FormContents
-                        submitError={submitError}
-                        submitting={submitting}
-                    />
-                </form>
-            )}
-        </Form>
+        <Loader
+            queryResponse={customAttributesQuery}
+            label={i18n.t('Custom attributes')}
+        >
+            <Form onSubmit={onSubmit} initialValues={initialValues}>
+                {({ handleSubmit, submitting, submitError }) => (
+                    <form onSubmit={handleSubmit}>
+                        <FormContents
+                            submitError={submitError}
+                            submitting={submitting}
+                        />
+                    </form>
+                )}
+            </Form>
+        </Loader>
     )
 }
 
