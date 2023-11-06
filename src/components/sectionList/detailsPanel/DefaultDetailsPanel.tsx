7fd98e3433bd9c5c228d36ebdafecaf1cb45c098
Birk Johansson <birk@dhis2.org>
2023-11-06 13:39:33 +0100

    fix(details): add createdBy, fallback to created if no lastUpdatedBy

--- src/components/sectionList/detailsPanel/DefaultDetailsPanel.tsx
+++ src/components/sectionList/detailsPanel/DefaultDetailsPanel.tsx
@@ -26,6 +26,7 @@ const defaultQueryFields = [
     'id',
     'shortName',
     'href',
+    'createdBy',
 ] as const
 
 const createDefaultDetailsQuery = (
@@ -87,11 +88,16 @@ const DetailsContent = ({ data }: { data: DetailsResponse }) => {
                     </DetailItem>
                 )}
                 <DetailItem label={i18n.t('Code')}>{data.code}</DetailItem>
+                <DetailItem label={i18n.t('Created by')}>
+                    {data.createdBy.displayName}
+                </DetailItem>
                 <DetailItem label={i18n.t('Created')}>
                     <ClientDateTime value={data.created} />
                 </DetailItem>
                 <DetailItem label={i18n.t('Last updated by')}>
-                    {data.lastUpdatedBy.displayName}
+                    {data.lastUpdatedBy
+                        ? data.lastUpdatedBy.displayName
+                        : data.createdBy.displayName}
                 </DetailItem>
                 <DetailItem label={i18n.t('Last updated')}>
                     <ClientDateTime value={data.lastUpdated} />
