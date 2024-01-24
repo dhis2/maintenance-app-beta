2dcbc75897011cce79d1222486b5d07a802112e4
Birk Johansson <birk@dhis2.org>
2024-01-24 22:46:06 +0100

    fix(filters): use refactored sectionlistfilter for refetch

--- src/components/sectionList/useSectionListParamsRefetch.tsx
+++ src/components/sectionList/useSectionListParamsRefetch.tsx
@@ -1,10 +1,10 @@
 import { useEffect } from 'react'
+import { useFilterQueryParams } from '../../lib'
 import { QueryRefetchFunction } from '../../types'
-import { useQueryParamsForModelGist } from './filters'
 
 /** Refetches when filter and pagination params change  */
 export const useSectionListParamsRefetch = (refetch: QueryRefetchFunction) => {
-    const params = useQueryParamsForModelGist()
+    const params = useFilterQueryParams()
 
     useEffect(() => {
         refetch({
