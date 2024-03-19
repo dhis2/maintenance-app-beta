f562d9e4ccb1345bec7d53ff435f23c44f8fa01c
Mohammer5 <jan-gerke@dhis2.org>
2024-03-19 08:21:39 +0800

    test(data element edit form): add cypress test

---
+++ cypress/e2e/dataElements/Edit.spec.ts
@@ -0,0 +1,88 @@
+describe('Data elements / Edit', () => {
+    it('should change a value', () => {
+        const now = Date.now()
+
+        cy.visit('/')
+
+        // Open data elements group in side nav
+        cy.get('[data-test="sidenav"] button:contains("Data elements")', {
+            timeout: 10000,
+        }).click()
+
+        // Navigate to data element list view
+        cy.get('[data-test="sidenav"] a:contains("Data element")')
+            .first() // the selector will also grab "Data element group" and "Data element group set"
+            .click()
+
+        // Go to Edit form
+        cy.get(
+            '[data-test="dhis2-uicore-datatablecell"]:contains("ANC 1st visit")'
+        )
+            .parents('tr')
+            .find('td:last-child a')
+            .click()
+
+        cy.get('[data-test="formfields-description"] textarea').should(
+            'not.have.value',
+            `description ${now}`
+        )
+
+        cy.get('[data-test="formfields-description"] textarea')
+            .clear()
+            .type(`description ${now}`)
+
+        // Submit form
+        cy.get('button:contains("Save and close")').click()
+
+        cy.contains('Data element management').should('exist')
+
+        // Go to Edit form
+        cy.get(
+            '[data-test="dhis2-uicore-datatablecell"]:contains("ANC 1st visit")'
+        )
+            .parents('tr')
+            .find('td:last-child a')
+            .click()
+
+        cy.get('[data-test="formfields-description"] textarea').should(
+            'have.value',
+            `description ${now}`
+        )
+    })
+
+    it('should not submit successfully when a required value has been removed', () => {
+        cy.visit('/')
+
+        // Open data elements group in side nav
+        cy.get('[data-test="sidenav"] button:contains("Data elements")', {
+            timeout: 10000,
+        }).click()
+
+        // Navigate to data element list view
+        cy.get('[data-test="sidenav"] a:contains("Data element")')
+            .first() // the selector will also grab "Data element group" and "Data element group set"
+            .click()
+
+        // Go to Edit form
+        cy.get(
+            '[data-test="dhis2-uicore-datatablecell"]:contains("ANC 1st visit")'
+        )
+            .parents('tr')
+            .find('td:last-child a')
+            .click()
+
+        cy.get('[data-test="formfields-name"] input').clear()
+
+        // Submit form
+        cy.get('button:contains("Save and close")').click()
+
+        // Assert error
+        cy.get('[data-test$="-validation"]:contains("Required")').should(
+            'have.length',
+            1
+        )
+        cy.get(
+            '[data-test="formfields-name-validation"]:contains("Required")'
+        ).should('exist')
+    })
+})
