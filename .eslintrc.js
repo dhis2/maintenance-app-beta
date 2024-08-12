dd2b5dbeddc1b9e4e3ec32c43cd052ff75cd71d6
Birk Johansson <birk@dhis2.org>
2024-08-12 16:07:56 +0200

    chore: fix eslint resolution

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
         'plugin:@typescript-eslint/recommended',
         'plugin:import/typescript',
     ],
+    // parser: '@typescript-eslint/parser',
     settings: {
         'import/resolver': {
             node: {
