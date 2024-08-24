4a3416b52963ac43e7e53ddeeba4d01493fa82cf
Birk Johansson <birk@dhis2.org>
2024-08-24 19:32:42 +0200

    fix: get defaults using zod

---
+++ src/lib/zod/getDefaults.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod'
+
+// from: https://github.com/colinhacks/zod/discussions/1953#discussioncomment-4811588
+export function getDefaults<Schema extends z.AnyZodObject>(schema: Schema) {
+    return Object.fromEntries(
+        Object.entries(schema.shape).map(([key, value]) => {
+            if (value instanceof z.ZodDefault)
+                return [key, value._def.defaultValue()]
+            return [key, undefined]
+        })
+    )
+}
