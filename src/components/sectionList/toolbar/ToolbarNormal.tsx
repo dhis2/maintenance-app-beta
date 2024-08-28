1f4fda6ab64d06ee45ecc12d21c2f59aeebed5ce
Birk Johansson <birk@dhis2.org>
2024-08-28 03:42:29 +0200

    fix: hide New model when it cannot be created

--- src/components/sectionList/toolbar/ToolbarNormal.tsx
+++ src/components/sectionList/toolbar/ToolbarNormal.tsx
@@ -3,7 +3,11 @@ import { Button, DataTableToolbar } from '@dhis2/ui'
 import { IconAdd24 } from '@dhis2/ui-icons'
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { routePaths } from '../../../lib'
+import {
+    routePaths,
+    useCanCreateModelInSection,
+    useModelSectionHandleOrThrow,
+} from '../../../lib'
 import { ManageListViewDialog } from '../listView/ManageListViewDialog'
 import css from './Toolbar.module.css'
 
@@ -15,15 +19,19 @@ export const ToolbarNormal = ({
     downloadButtonElement,
 }: ToolbarNormalProps) => {
     const [manageColumnsOpen, setManageColumnsOpen] = React.useState(false)
+    const section = useModelSectionHandleOrThrow()
+    const canCreateModel = useCanCreateModelInSection(section)
 
     const handleClose = () => setManageColumnsOpen(false)
     return (
         <DataTableToolbar className={css.listHeaderNormal}>
-            <Link to={routePaths.sectionNew}>
-                <Button small icon={<IconAdd24 />}>
-                    {i18n.t('New')}
-                </Button>
-            </Link>
+            {canCreateModel && (
+                <Link to={routePaths.sectionNew}>
+                    <Button small icon={<IconAdd24 />}>
+                        {i18n.t('New')}
+                    </Button>
+                </Link>
+            )}
             {downloadButtonElement}
             <Button small onClick={() => setManageColumnsOpen((prev) => !prev)}>
                 {i18n.t('Manage View')}
