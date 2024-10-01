70b5437a27c6973ad19a6f6cd6d11c8111c96c4e
Birk Johansson <birk@dhis2.org>
2024-10-01 16:20:05 +0200

    fix: make filters optional

--- src/pages/categoryOptionCombos/List.tsx
+++ src/pages/categoryOptionCombos/List.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { DefaultSectionList } from '../DefaultSectionList'
 
-export const Component = () => <DefaultSectionList />
+export const Component = DefaultSectionList
