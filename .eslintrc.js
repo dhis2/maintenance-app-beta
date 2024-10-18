bf8b8656eba6f4fa9a65721e6091ff21407b12ad
Mozafar Haider <mozafar@dhis2.org>
2024-10-18 07:53:34 +0100

    chore: update d2-style to support typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,9 @@
-// eslint-disable-next-line @typescript-eslint/no-require-imports
 const { config } = require('@dhis2/cli-style')
 
 module.exports = {
-    extends: [
-        config.eslintReact,
-        'plugin:@typescript-eslint/recommended',
-        'plugin:import/typescript',
-    ],
-    settings: {
-        'import/resolver': {
-            node: {
-                extensions: ['.js', '.jsx', '.ts', '.tsx'],
-            },
-        },
-    },
+    extends: [config.eslintReact],
     rules: {
-        'import/extensions': 'off',
         '@typescript-eslint/no-explicit-any': 1,
-        '@typescript-eslint/no-unused-vars': 1,
+        //     '@typescript-eslint/no-unused-vars': 1,
     },
 }
