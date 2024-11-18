c2481367f6db31c44e542ad35bb974e5eeb9c89c
Birk Johansson <birk@dhis2.org>
2024-11-18 13:45:02 +0100

    feat: add merge button to toolbar

---
+++ src/lib/sections/mergableSections.ts
@@ -0,0 +1,40 @@
+import { ModelSection } from '../../types'
+import { SECTIONS_MAP } from '../constants'
+import { useModelSectionHandleOrThrow } from '../routeUtils'
+import { hasAuthority, useCurrentUserAuthorities } from '../user'
+
+const sectionAuthorityMap = new Map<ModelSection, string>([
+    [SECTIONS_MAP.indicator, 'F_INDICATOR_MERGE'],
+    [SECTIONS_MAP.indicatorType, 'F_INDICATOR_TYPE_MERGE'],
+])
+
+const getMergeAuthority = (section: ModelSection): string | undefined => {
+    return sectionAuthorityMap.get(section)
+}
+
+export const isMergableSection = (section: ModelSection): boolean => {
+    return getMergeAuthority(section) !== undefined
+}
+
+const canMergeModelInSection = (
+    section: ModelSection,
+    userAuthorities: Set<string>
+): boolean => {
+    const authority = getMergeAuthority(section)
+
+    if (authority) {
+        return hasAuthority(userAuthorities, authority)
+    }
+    return false
+}
+
+export const useCanMergeModelInSection = (section: ModelSection) => {
+    const userAuthorities = useCurrentUserAuthorities()
+
+    return canMergeModelInSection(section, userAuthorities)
+}
+
+export const useCanMergeModelInCurrentSection = () => {
+    const section = useModelSectionHandleOrThrow()
+    return useCanMergeModelInSection(section)
+}
