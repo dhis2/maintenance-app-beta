5ee158964aabd02f391bf8dbd908534960c7ec14
Chisomchima <chimachisom360@gmail.com>
2024-11-25 06:51:11 +0100

    feat: add description validation

--- src/components/form/fields/DescriptionField.tsx
+++ src/components/form/fields/DescriptionField.tsx
@@ -1,8 +1,46 @@
 import i18n from '@dhis2/d2-i18n'
 import { TextAreaFieldFF } from '@dhis2/ui'
-import React from 'react'
-import { Field as FieldRFF } from 'react-final-form'
-import { SchemaSection, useCheckMaxLengthFromSchema } from '../../../lib'
+import React, { useMemo } from 'react'
+import { Field as FieldRFF, useField } from 'react-final-form'
+import { useParams } from 'react-router-dom'
+import {
+    composeAsyncValidators,
+    required,
+    useCheckMaxLengthFromSchema,
+    useIsFieldValueUnique,
+    SchemaSection,
+} from '../../../lib'
+
+function useValidator({
+    schemaSection,
+    fieldName,
+}: {
+    schemaSection: SchemaSection
+    fieldName: string
+}) {
+    const params = useParams()
+    const modelId = params.id as string
+    const checkIsValueTaken = useIsFieldValueUnique({
+        model: schemaSection.namePlural,
+        field: fieldName,
+        id: modelId,
+    })
+
+    const checkMaxLength = useCheckMaxLengthFromSchema(
+        schemaSection.name,
+        fieldName
+    )
+
+    return useMemo(
+        () =>
+            composeAsyncValidators<string>([
+                checkIsValueTaken,
+                checkMaxLength,
+                required,
+            ]),
+        [checkIsValueTaken, checkMaxLength]
+    )
+}
 
 export function DescriptionField({
     helpText,
@@ -11,17 +49,27 @@ export function DescriptionField({
     helpText?: string
     schemaSection: SchemaSection
 }) {
-    const validate = useCheckMaxLengthFromSchema(schemaSection.name, 'formName')
+    const validator = useValidator({ schemaSection, fieldName: 'description' })
+    const { meta } = useField('description', {
+        subscription: { validating: true },
+    })
+
+    const helpString =
+        helpText ||
+        i18n.t('A description should be clear and provide necessary details.')
 
     return (
-        <FieldRFF
+        <FieldRFF<string | undefined>
+            loading={meta.validating}
             component={TextAreaFieldFF}
             dataTest="formfields-description"
             inputWidth="400px"
             name="description"
-            label={i18n.t('Description')}
-            helpText={helpText}
-            validate={validate}
+            label={i18n.t('Description (required)', {
+                fieldLabel: i18n.t('Description'),
+            })}
+            helpText={helpString}
+            validate={(description?: string) => validator(description)}
             validateFields={[]}
         />
     )
