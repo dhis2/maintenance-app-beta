d78455af2bcb082d5e2a5e4bb6651e8f9157ab99
Chisomchima <chimachisom360@gmail.com>
2024-11-25 22:37:56 +0100

    chore: add extra validation for indicator types forms

--- src/pages/indicatorTypes/form/IndicatorTypesSchema.ts
+++ src/pages/indicatorTypes/form/IndicatorTypesSchema.ts
@@ -4,7 +4,14 @@ import { getDefaults, createFormValidate, modelFormSchemas } from '../../../lib'
 const { identifiable } = modelFormSchemas
 
 export const IndicatorSchema = identifiable.extend({
-    factor: z.coerce.number().int(),
+    factor: z.coerce
+        .number({ invalid_type_error: 'Please enter a number' })
+        .int()
+        .min(1, 'Please enter a value above 0')
+        .max(
+            Number.MAX_SAFE_INTEGER,
+            `The number is too large. Please enter a valid integer.`
+        ),
 })
 
 export const initialValues = getDefaults(IndicatorSchema)
