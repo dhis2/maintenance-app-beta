66da56f0716da42d189ebd67de0e0c7a17029743
Chisomchima <chimachisom360@gmail.com>
2024-11-27 12:32:30 +0100

    chore: update schema

--- src/pages/indicatorTypes/form/IndicatorTypesSchema.ts
+++ src/pages/indicatorTypes/form/IndicatorTypesSchema.ts
@@ -7,11 +7,13 @@ export const IndicatorSchema = identifiable.extend({
     factor: z.coerce
         .number({ invalid_type_error: 'Please enter a number' })
         .int()
-        .min(1, 'Please enter a value above 0')
         .max(
             Number.MAX_SAFE_INTEGER,
             `The number is too large. Please enter a valid integer.`
-        ),
+        )
+        .refine((value) => value !== 0, {
+            message: 'Zero is not a valid value for factor',
+        }),
 })
 
 export const initialValues = getDefaults(IndicatorSchema)
