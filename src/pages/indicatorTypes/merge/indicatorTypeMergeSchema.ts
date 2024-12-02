7ad0483f1c647c5aec267eb0c3a47e28fa884939
Birk Johansson <birk@dhis2.org>
2024-12-02 19:16:37 +0100

    fix: add source-target fields

--- src/pages/indicatorTypes/merge/indicatorTypeMergeSchema.ts
+++ src/pages/indicatorTypes/merge/indicatorTypeMergeSchema.ts
@@ -3,14 +3,10 @@ import { createFormValidate, modelFormSchemas } from '../../../lib'
 
 const { referenceCollection } = modelFormSchemas
 
-export const mergeFormSchema = z
-    .object({
-        sources: referenceCollection.default([]),
-        target: referenceCollection.length(1).default([]),
-        deleteSources: z.boolean().default(false),
-    })
-    .refine((data) => {
-        return 'error!'
-    })
+export const mergeFormSchema = z.object({
+    sources: referenceCollection.default([]),
+    target: referenceCollection.length(1).default([]),
+    deleteSources: z.enum(['keep', 'delete']).default('keep'),
+})
 
 export const validate = createFormValidate(mergeFormSchema)
