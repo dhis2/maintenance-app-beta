07b12a33814916af9eff35e37e769053898f1f9b
Flaminia Cavallo <flaminia@dhis2.org>
2024-12-19 14:25:32 +0100

    feat: translate period types

--- src/pages/dataSetsWip/form/PeriodTypeField.tsx
+++ src/pages/dataSetsWip/form/PeriodTypeField.tsx
@@ -5,9 +5,30 @@ import { useField } from 'react-final-form'
 import { useQuery } from 'react-query'
 import { SearchableSingleSelect } from '../../../components'
 import { useBoundResourceQueryFn } from '../../../lib/query/useBoundQueryFn'
-import {Category, PickWithFieldFilters} from "../../../types/generated";
 
-export type PeriodTypes = {periodTypes: [{name:string}]}
+export type PeriodTypes = { periodTypes: [{ name: string }] }
+const periodTypesToTranslatedValues: Record<string, string> = {
+    BiMonthly: i18n.t('BiMonthly'),
+    BiWeekly: i18n.t('BiWeekly'),
+    Daily: i18n.t('Daily'),
+    FinancialApril: i18n.t('FinancialApril'),
+    FinancialJuly: i18n.t('FinancialJuly'),
+    FinancialNov: i18n.t('FinancialNov'),
+    FinancialOct: i18n.t('FinancialOct'),
+    Monthly: i18n.t('Monthly'),
+    Quarterly: i18n.t('Quarterly'),
+    QuarterlyNov: i18n.t('QuarterlyNov'),
+    SixMonthlyApril: i18n.t('SixMonthlyApril'),
+    SixMonthlyNov: i18n.t('SixMonthlyNov'),
+    SixMonthly: i18n.t('SixMonthly'),
+    TwoYearly: i18n.t('TwoYearly'),
+    Weekly: i18n.t('Weekly'),
+    WeeklySaturday: i18n.t('WeeklySaturday'),
+    WeeklySunday: i18n.t('WeeklySunday'),
+    WeeklyThursday: i18n.t('WeeklyThursday'),
+    WeeklyWednesday: i18n.t('WeeklyWednesday'),
+    Yearly: i18n.t('Yearly'),
+}
 
 export function PeriodTypeField() {
     const { input, meta } = useField('periodType')
@@ -28,7 +49,8 @@ export function PeriodTypeField() {
     const options = useMemo(() => {
         return data
             ? data.periodTypes.map((period) => ({
-                  label: i18n.t(period.name),
+                  label:
+                      periodTypesToTranslatedValues[period.name] || period.name,
                   value: period.name,
               }))
             : []
@@ -39,12 +61,12 @@ export function PeriodTypeField() {
             required
             name="periodType"
             label={i18n.t('Period type')}
-            error={(meta.touched && !!meta.error)}
+            error={meta.touched && !!meta.error}
             validationText={meta.touched ? meta.error : undefined}
         >
             <SearchableSingleSelect
                 selected={input.value}
-                invalid={(meta.touched && !!meta.error)}
+                invalid={meta.touched && !!meta.error}
                 error={error ? i18n.t('Something went wrong') : undefined}
                 onChange={({ selected }) => input.onChange(selected)}
                 searchable={false}
