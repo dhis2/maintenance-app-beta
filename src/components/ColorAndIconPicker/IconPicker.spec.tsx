4668167dc4364c6fc15c949167a2f2030694fb3f
Birk Johansson <birk@dhis2.org>
2025-01-22 14:40:42 +0100

    fix(icons): update icons picker to support paginated api (#485) [skip release]
    * fix(icons): implement paginated icons list
    
    * fix(icons): remove filterIcons
    This is now handled server-side and not needed
    
    * fix(icons): cleanup and translate tabs
    
    * fix: iconmessage container
    
    * fix: add tests - do not load icon if not present
    
    * fix: add missing iconmocks
    
    * fix: fix failing tests by reverting dataTest attr

---
+++ src/components/ColorAndIconPicker/IconPicker.spec.tsx
@@ -0,0 +1,116 @@
+import { render, screen, waitFor } from '@testing-library/react'
+import { userEvent } from '@testing-library/user-event'
+import React from 'react'
+import '@testing-library/jest-dom'
+import iconsMock from '../../__mocks__/iconsMock.json'
+import { ComponentWithProvider, CustomData } from '../../testUtils'
+import { ResourceQuery } from '../../types'
+import { IconPicker } from './IconPicker'
+
+const resolvers = {
+    icons: (type: string, options: ResourceQuery) => {
+        if (options.id) {
+            return Promise.resolve(
+                iconsMock.icons.find((icon) => icon.key === options.id)
+            )
+        }
+        return Promise.resolve(iconsMock)
+    },
+} satisfies CustomData
+const WithProvider = ({ children }: React.PropsWithChildren) => (
+    <ComponentWithProvider dataForCustomProvider={resolvers}>
+        {children}
+    </ComponentWithProvider>
+)
+
+describe('IconPicker', () => {
+    const mockOnIconPick = jest.fn()
+
+    it('renders the IconPicker with no selected icon', () => {
+        render(<IconPicker icon="test" onIconPick={mockOnIconPick} />, {
+            wrapper: WithProvider,
+        })
+
+        const iconButton = screen.getByTestId('iconpicker-trigger')
+        expect(iconButton).toBeInTheDocument()
+        expect(iconButton.children).not.toContain('IMG')
+        expect(screen.queryByAltText('test')).not.toBeInTheDocument()
+    })
+
+    it('renders the selected icon when provided', async () => {
+        const testIcon = iconsMock.icons[0]
+        render(<IconPicker onIconPick={mockOnIconPick} icon={testIcon.key} />, {
+            wrapper: WithProvider,
+        })
+
+        await waitFor(() => {
+            const image = screen.getByAltText(testIcon.key)
+            expect(image).toBeInTheDocument()
+            expect(image.tagName).toBe('IMG')
+            expect(image).toHaveAttribute('src', testIcon.href)
+        })
+    })
+
+    it('opens the IconPickerModal when the button is clicked', async () => {
+        render(<IconPicker onIconPick={mockOnIconPick} />, {
+            wrapper: WithProvider,
+        })
+
+        const triggerButton = screen.getByTestId('iconpicker-trigger')
+        await userEvent.click(triggerButton)
+
+        expect(screen.getByTestId('dhis2-uicore-modal')).toBeInTheDocument()
+        const icon = await screen.findByAltText(iconsMock.icons[0].key)
+        expect(icon).toBeInTheDocument()
+    })
+
+    it('closes the IconPickerModal when the cancel button is clicked', async () => {
+        const alreadySelectedIcon = iconsMock.icons[0].key
+        render(
+            <IconPicker
+                onIconPick={mockOnIconPick}
+                icon={alreadySelectedIcon}
+            />,
+            {
+                wrapper: WithProvider,
+            }
+        )
+
+        const triggerButton = screen.getByTestId('iconpicker-trigger')
+        await userEvent.click(triggerButton)
+
+        const cancelButton = await screen.findByText('Cancel')
+        await userEvent.click(cancelButton)
+        expect(mockOnIconPick).not.toHaveBeenCalled()
+        expect(
+            screen.queryByTestId('dhis2-uicore-modal')
+        ).not.toBeInTheDocument()
+    })
+
+    it('calls onIconPick with icon-key and closes the modal when an icon is selected', async () => {
+        render(
+            <IconPicker
+                onIconPick={mockOnIconPick}
+                icon={iconsMock.icons[0].key}
+            />,
+            {
+                wrapper: WithProvider,
+            }
+        )
+
+        const newIconKey = iconsMock.icons[3]
+        const triggerButton = screen.getByTestId('iconpicker-trigger')
+        await userEvent.click(triggerButton)
+
+        const newIcon = await screen.findByTitle(newIconKey.key)
+        await userEvent.click(newIcon)
+
+        const selectButton = await screen.findByText('Select')
+        await userEvent.click(selectButton)
+
+        expect(mockOnIconPick).toHaveBeenCalledWith({ icon: newIconKey.key })
+        expect(
+            screen.queryByTestId('dhis2-uicore-modal')
+        ).not.toBeInTheDocument()
+    })
+})
