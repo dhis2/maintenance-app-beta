09685e425a97050d795f050ae1f014cf9bee4414
Thomas Zemp <tom@dhis2.org>
2025-01-22 08:55:00 +0100

    feat: add test placeholder

---
+++ src/pages/dataSetsWip/New.spec.tsx
@@ -0,0 +1,8 @@
+describe.skip('Data sets add', () => {
+    it('should let you add and save data sets', () => {
+        // mock api/dataElements
+        // select data elements, check UI
+        // unselect data elements, check UI
+        // save, assert that payload is correct (i.e. submitted with dataSetElements)
+    })
+})
