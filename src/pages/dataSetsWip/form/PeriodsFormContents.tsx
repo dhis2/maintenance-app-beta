328c206b16fca0178df58f61bbac9e91d576cc93
Flaminia <fl.cavallo@gmail.com>
2025-01-27 08:57:59 +0100

    feat: data set period fields (#496)
    * feat: add checkbox fields to period section of data set form
    
    * feat: add test specs
    
    * fix: lint
    
    * fix: rename component, use hook to get form values, fix bug with checkbox state
    
    * fix: remove unused value
    
    * fix: rename component and small refactor
    
    * fix: lint

--- src/pages/dataSetsWip/form/PeriodsFormContents.tsx
+++ src/pages/dataSetsWip/form/PeriodsFormContents.tsx
@@ -1,13 +1,18 @@
 import i18n from '@dhis2/d2-i18n'
 import React from 'react'
+import { useFormState } from 'react-final-form'
 import {
+    StandardFormField,
     StandardFormSectionDescription,
     StandardFormSectionTitle,
 } from '../../../components'
 import { SectionedFormSection } from '../../../components/sectionedForm'
+import { DEFAULT_CATEGORY_COMBO } from '../../../lib'
 import { PeriodTypeField } from './PeriodTypeField'
+import { ToggledNumberInput } from './ToggledNumberInput'
 
 export const PeriodsContents = ({ name }: { name: string }) => {
+    const formValues = useFormState().values
     return (
         <SectionedFormSection name={name}>
             <StandardFormSectionTitle>
@@ -18,7 +23,37 @@ export const PeriodsContents = ({ name }: { name: string }) => {
                     'Choose for what time periods data can be entered for this data set'
                 )}
             </StandardFormSectionDescription>
-            <PeriodTypeField />
+            <StandardFormField>
+                <PeriodTypeField />
+            </StandardFormField>
+            <StandardFormField>
+                <ToggledNumberInput
+                    name="openFuturePeriods"
+                    label={i18n.t('Allow data entry for future periods')}
+                    uncheckedValue={0}
+                    min={'1'}
+                />
+            </StandardFormField>
+            <StandardFormField>
+                <ToggledNumberInput
+                    name="expiryDays"
+                    label={i18n.t(
+                        'Close data entry a certain number of days after period end (expiry days)'
+                    )}
+                    uncheckedValue={0.0}
+                />
+            </StandardFormField>
+            {formValues?.categoryCombo.id !== DEFAULT_CATEGORY_COMBO.id && (
+                <StandardFormField>
+                    <ToggledNumberInput
+                        name="openPeriodsAfterCoEndDate"
+                        label={i18n.t(
+                            `Close data entry after ${formValues?.categoryCombo.displayName.toLowerCase()} category option end date (if category option exist and has end date)`
+                        )}
+                        uncheckedValue={0.0}
+                    />
+                </StandardFormField>
+            )}
         </SectionedFormSection>
     )
 }
--- src/pages/dataSetsWip/form/dataSetFormSchema.ts
+++ src/pages/dataSetsWip/form/dataSetFormSchema.ts
@@ -1,3 +1,4 @@
+import i18n from '@dhis2/d2-i18n'
 import { z } from 'zod'
 import {
     DEFAULT_CATEGORY_COMBO,
@@ -21,8 +22,15 @@ export const dataSetFormSchema = identifiable
             .object({ id: z.string(), displayName: z.string() })
             .default({ ...DEFAULT_CATEGORY_COMBO }),
         periodType: z.string().default('Monthly'),
+        openFuturePeriods: z
+            .number()
+            .int({ message: i18n.t('The number should not have decimals') }),
+        expiryDays: z.number(),
+        openPeriodsAfterCoEndDate: z.number().optional(),
     })
 
 export const initialValues = getDefaults(dataSetFormSchema)
+
+export type DataSetFormValues = typeof initialValues
 
 export const validate = createFormValidate(dataSetFormSchema)
