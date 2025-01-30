ac7fd142e6c0680b175213a69a8b7202ec776e53
Flaminia Cavallo <flaminia@dhis2.org>
2025-01-30 11:57:12 +0100

    feat: temp org unit level selector in datasets

--- src/components/form/fields/OrganisationUnitField.tsx
+++ src/components/form/fields/OrganisationUnitField.tsx
@@ -4,16 +4,18 @@ import {
     OrganisationUnitTree,
     OrganisationUnitTreeProps,
 } from '@dhis2/ui'
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { useField } from 'react-final-form'
 import { useCurrentUserRootOrgUnits } from '../../../lib/user/currentUserStore'
 import classes from './OrganisationUnitField.module.css'
+import {ModelMultiSelect} from "../../metadataFormControls";
 
 type OrganisationUnitFieldProps = {
     name?: string
     label?: string
     singleSelection?: boolean
     onChange?: (orgUnits: OrganisationUnitFormValue[]) => void
+    withLevelSelector?: boolean
 }
 
 export type OrganisationUnitFormValue = {
@@ -22,11 +24,20 @@ export type OrganisationUnitFormValue = {
     displayName: string
 }
 
+const orgUnitLevelQuery = {
+    resource: 'organisationUnitLevels',
+    params: {
+        fields: ['id', 'displayName', 'level'],
+        order: 'level:asc',
+    },
+}
+
 export const OrganisationUnitField = ({
     name,
     onChange,
     label,
     singleSelection = false,
+    withLevelSelector = false,
 }: OrganisationUnitFieldProps) => {
     const { input, meta } = useField<
         OrganisationUnitFormValue[] | '',
@@ -37,8 +48,28 @@ export const OrganisationUnitField = ({
     })
 
     const roots = useCurrentUserRootOrgUnits()
-
     const rootIds = roots.map((ou) => ou.id)
+    const queryFn = useBoundResourceQueryFn()
+    const [orgUnitLevels, setOrgUnitLevels] = useState([])
+
+    const levelQuery = useQuery({
+        queryKey: [
+            {
+                resource: 'organisationUnits',
+                params: {
+                    fields: ['id', 'displayName', 'path'],
+                    filter: [
+                        `level:in:[${orgUnitLevels
+                            .map((level) => level.level)
+                            .join(',')}]`,
+                    ],
+                },
+            },
+        ],
+        queryFn: queryFn<OrganisationUnitFormValue>,
+        enabled: orgUnitLevels.length > 0,
+    })
+    console.log('*************DATA', levelQuery?.data)
 
     const handleChange: OrganisationUnitTreeProps['onChange'] = ({
         selected,
@@ -78,6 +109,15 @@ export const OrganisationUnitField = ({
                     initiallyExpanded={rootIds}
                 />
             </div>
+            {withLevelSelector && (
+                <ModelMultiSelect
+                    query={orgUnitLevelQuery}
+                    onChange={(orgUnits) => {
+                        setOrgUnitLevels(orgUnits.selected)
+                    }}
+                    selected={orgUnitLevels}
+                />
+            )}
         </Field>
     )
 }
