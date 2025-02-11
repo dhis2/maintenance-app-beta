261f80228c009d702c2baf493f1b91726f8f6109
Milagros <milagros@dhis2.org>
2025-02-11 11:12:47 +0100

    feat: timely input default value

--- src/pages/dataSetsWip/form/dataSetFormSchema.ts
+++ src/pages/dataSetsWip/form/dataSetFormSchema.ts
@@ -48,7 +48,7 @@ export const dataSetFormSchema = identifiable
         fieldCombinationRequired: z.boolean().default(false),
         compulsoryFieldsCompleteOnly: z.boolean().default(false),
         workflow: z.object({ id: z.string() }).optional(),
-        timelyDays: z.number().optional(),
+        timelyDays: z.number().optional().default(15),
         compulsoryDataElementOperands: z
             .array(
                 z.object({
