0ce56ba2aa06f12d9ab2bb0cc590ec3d5b09f7d4
Birk Johansson <birk@dhis2.org>
2025-02-26 16:25:06 +0100

    refactor: cleanup

--- src/pages/dataSetsWip/form/OrganisationUnitsFormContents.tsx
+++ src/pages/dataSetsWip/form/OrganisationUnitsFormContents.tsx
@@ -7,11 +7,9 @@ import {
     StandardFormSectionTitle,
 } from '../../../components'
 import { SectionedFormSection } from '../../../components/sectionedForm'
-import classes from './OrganisationUnitsFormContents.module.css'
 
 export const OrganisationUnitsFormContents = React.memo(
     function OrganisationUnitsFormContents({ name }: { name: string }) {
-        console.log('render')
         return (
             <SectionedFormSection name={name}>
                 <StandardFormSectionTitle>
@@ -22,27 +20,9 @@ export const OrganisationUnitsFormContents = React.memo(
                         'Configure which organisation units can collect data for this data set.'
                     )}
                 </StandardFormSectionDescription>
-                <div className={classes.organisationUnitSelectorWrapper}>
-                    <StandardFormField>
-                        <OrganisationUnitTreeWithToolbarFormField />
-                    </StandardFormField>
-                    {/* <div className={classes.organisationUnitSelectorRhs}>
-                        <h4>
-                            {!formValues[fieldName] ||
-                            formValues[fieldName]?.length === 0
-                                ? i18n.t('No selected units')
-                                : i18n.t('{{numberOfUnits}} selected units', {
-                                      numberOfUnits:
-                                          formValues[fieldName]?.length,
-                                  })}
-                        </h4>
-                        {formValues[fieldName]?.map(
-                            (orgUnit: OrganisationUnitFormValue) => (
-                                <p key={orgUnit.id}>{orgUnit.displayName}</p>
-                            )
-                        )}
-                    </div> */}
-                </div>
+                <StandardFormField>
+                    <OrganisationUnitTreeWithToolbarFormField />
+                </StandardFormField>
             </SectionedFormSection>
         )
     }
