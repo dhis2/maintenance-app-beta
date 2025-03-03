4f0703bac42d07d96f82605459a6a488ece128df
Thomas Zemp <tom@dhis2.org>
2025-03-03 14:51:37 +0100

    feat: add data sets and org units in overview section [DHIS2-18953] (#523)

--- src/pages/overview/AllOverview.tsx
+++ src/pages/overview/AllOverview.tsx
@@ -2,13 +2,17 @@ import i18n from '@dhis2/d2-i18n'
 import React from 'react'
 import { CategoryCardGroup } from './Categories'
 import { DataElementsCardGroup } from './DataElements'
+import { DataSetsCardGroup } from './DataSets'
 import { OverviewGroup } from './group'
+import { OrganisationUnitsCardGroup } from './OrganisationUnits'
 
 export const AllOverview = () => {
     return (
         <OverviewGroup title={i18n.t('Metadata management')}>
             <DataElementsCardGroup showTitle={true} />
             <CategoryCardGroup showTitle={true} />
+            <DataSetsCardGroup showTitle={true} />
+            <OrganisationUnitsCardGroup showTitle={true} />
         </OverviewGroup>
     )
 }
