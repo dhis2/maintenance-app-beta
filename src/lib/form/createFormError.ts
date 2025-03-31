2faa248f7daf8a39f841b5e9758f87fcc51c04ad
Birk Johansson <birk@dhis2.org>
2025-03-31 15:50:52 +0200

    feat(forms): improve display of errors (#535)
    * feat: add error repsponse parser
    
    * feat: better error in forms
    
    * fix: cleanup

--- src/lib/form/createFormError.ts
+++ src/lib/form/createFormError.ts
@@ -1,11 +1,16 @@
 import { FORM_ERROR } from 'final-form'
+import { ApiErrorReport, isApiErrorReport, parseErrorResponse } from '../errors'
 
-export const createFormError = (error: unknown) => {
-    let errorMessage
-    if (error instanceof Error || typeof error === 'string') {
-        errorMessage = error.toString()
+export const createFormError = (
+    error: unknown
+): {
+    [FORM_ERROR]: ApiErrorReport
+} => {
+    let resolvedError: ApiErrorReport
+    if (isApiErrorReport(error)) {
+        resolvedError = error
     } else {
-        errorMessage = 'An unknown error occurred'
+        resolvedError = parseErrorResponse(error)
     }
-    return { [FORM_ERROR]: errorMessage }
+    return { [FORM_ERROR]: resolvedError }
 }
