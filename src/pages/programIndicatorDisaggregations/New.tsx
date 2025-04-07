3f2ad248718754b2195c50e7584f53e3c1c48379
Thomas Zemp <tom@dhis2.org>
2025-04-07 16:51:29 +0200

    fix: updates for specs

--- src/pages/programIndicatorDisaggregations/New.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Component = () => {
-    return (
-        <div>
-            <span>Placeholder for program indicator disaggregations (new)</span>
-        </div>
-    )
-}
