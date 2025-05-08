d418387ec7aa3f33e1f99d91c16f4b0a19b828d8
Birk Johansson <birk@dhis2.org>
2025-05-08 16:39:46 +0200

    fix(sectioned-sidebar): update sidebar looks

--- src/components/sectionedForm/DefaultSectionedFormFooter.tsx
+++ src/components/sectionedForm/DefaultSectionedFormFooter.tsx
@@ -2,7 +2,6 @@ import i18n from '@dhis2/d2-i18n'
 import { Button } from '@dhis2/ui'
 import React from 'react'
 import { useForm } from 'react-final-form'
-import { useSectionedFormContext, useSelectedSection } from '../../lib'
 import { LinkButton } from '../LinkButton'
 import { SectionedFormFooter } from './SectionedFormFooter'
 
@@ -11,45 +10,10 @@ export const DefaultSectionedFormFooter = ({
 }: {
     submitting?: boolean
 }) => {
-    const descriptor = useSectionedFormContext()
-    const [selected, setSelectedSection] = useSelectedSection()
-
     const { submit } = useForm()
-    const currSelectedIndex = descriptor.sections.findIndex(
-        (s) => s.name === selected
-    )
-    const prevSection = descriptor.sections[currSelectedIndex - 1]
-    const nextSection = descriptor.sections[currSelectedIndex + 1]
-
-    const handleNavigateBack = () => {
-        if (prevSection) {
-            setSelectedSection(prevSection.name)
-        }
-    }
-    const handleNavigateNext = () => {
-        if (nextSection) {
-            setSelectedSection(nextSection.name)
-        }
-    }
 
     return (
         <SectionedFormFooter>
-            <SectionedFormFooter.SectionActions>
-                {prevSection && (
-                    <Button small onClick={handleNavigateBack}>
-                        {i18n.t('Go back')}
-                    </Button>
-                )}
-                {nextSection && (
-                    <Button
-                        style={{ marginInlineStart: 'auto' }}
-                        small
-                        onClick={handleNavigateNext}
-                    >
-                        {i18n.t('Next section')}
-                    </Button>
-                )}
-            </SectionedFormFooter.SectionActions>
             <SectionedFormFooter.FormActions>
                 <Button
                     primary
