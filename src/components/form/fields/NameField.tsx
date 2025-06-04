4aab48032670cf7a9185613c51b530182337f8f6
Chisomchima <chimachisom360@gmail.com>
2025-06-04 09:21:34 +0100

    refactor: simplify NameField component by using useForm

--- src/components/form/fields/NameField.tsx
+++ src/components/form/fields/NameField.tsx
@@ -1,7 +1,7 @@
 import i18n from '@dhis2/d2-i18n'
 import { InputFieldFF } from '@dhis2/ui'
 import React from 'react'
-import { Field as FieldRFF, useField } from 'react-final-form'
+import { Field as FieldRFF, useForm } from 'react-final-form'
 import { SchemaSection } from '../../../lib'
 import { useValidator } from '../../../lib/models/useFieldValidators'
 
@@ -15,40 +15,41 @@ export function NameField({
     extraValidator?: (value?: string) => Promise<string | undefined> | undefined
 }) {
     const validator = useValidator({ schemaSection, property: 'name' })
-    const { meta } = useField('name', {
-        subscription: { validating: true },
-    })
+    const form = useForm()
 
     const helpString =
         helpText || i18n.t('A name should be concise and easy to recognize.')
 
-    const validate = async (name?: string) => {
-        const error = await validator(name)
-        if (error) {
-            return error
-        }
-
-        if (extraValidator) {
-            return await extraValidator(name)
-        }
-
-        return undefined
-    }
-
     return (
-        <FieldRFF<string | undefined>
-            loading={meta.validating}
-            component={InputFieldFF}
-            dataTest="formfields-name"
-            required
-            inputWidth="400px"
-            label={i18n.t('{{fieldLabel}} (required)', {
-                fieldLabel: i18n.t('Name'),
-            })}
-            name="name"
-            helpText={helpString}
-            validate={validate}
-            validateFields={[]}
-        />
+        <FieldRFF name="name" validate={validator}>
+            {({ input, meta }) => (
+                <InputFieldFF
+                    input={{
+                        ...input,
+                        onChange: async (value: string) => {
+                            input.onChange(value)
+
+                            if (extraValidator) {
+                                const warning = await extraValidator(value)
+                                form.change('nameWarning', warning)
+                            }
+                        },
+                    }}
+                    meta={meta}
+                    dataTest="formfields-name"
+                    required
+                    inputWidth="400px"
+                    label={i18n.t('{{fieldLabel}} (required)', {
+                        fieldLabel: i18n.t('Name'),
+                    })}
+                    helpText={helpString}
+                    validationText={
+                        form.getState().values.nameWarning &&
+                        i18n.t('This name may not be unique')
+                    }
+                    warning={!!form.getState().values.nameWarning}
+                />
+            )}
+        </FieldRFF>
     )
 }
