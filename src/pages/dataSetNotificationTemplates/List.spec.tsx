0317984fdc277d8fc984026ba29da320c7236e68
Chisomchima <chimachisom360@gmail.com>
2025-06-16 08:06:16 +0100

    refactor: simplify List.spec.tsx by removing unused testDataSet and customData structure

--- src/pages/dataSetNotificationTemplates/List.spec.tsx
+++ src/pages/dataSetNotificationTemplates/List.spec.tsx
@@ -1,9 +1,6 @@
 import dataSetNotificationTemplateSchemaMock from '../../__mocks__/schema/dataSetNotificationTemplatesSchema.json'
 import { SECTIONS_MAP } from '../../lib'
-import {
-    testDataSetNotificationTemplate,
-    testDataSet,
-} from '../../testUtils/builders'
+import { testDataSetNotificationTemplate } from '../../testUtils/builders'
 import { generateDefaultListTests } from '../defaultListTests'
 import { Component as DataSetNotificationTemplateList } from './List'
 
@@ -11,16 +8,7 @@ const section = SECTIONS_MAP.dataSetNotificationTemplate
 const mockSchema = dataSetNotificationTemplateSchemaMock
 const ComponentToTest = DataSetNotificationTemplateList
 const generateRandomElement = testDataSetNotificationTemplate
-const customData = {
-    dataSets: (type: any, params: any) => {
-        if (type === 'read') {
-            return {
-                dataSets: [testDataSet(), testDataSet(), testDataSet()],
-                pager: { page: 1, total: 3, pageSize: 20, pageCount: 1 },
-            }
-        }
-    },
-}
+const customData = {}
 
 generateDefaultListTests({
     section,
