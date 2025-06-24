6cc7076d432e77904539827bdaf72e3a3eb32220
Flaminia Cavallo <flaminia@dhis2.org>
2025-06-24 06:31:56 -0700

    fix: remove unused imports

--- src/pages/indicatorTypes/form/IndicatorTypesFormFields.tsx
+++ src/pages/indicatorTypes/form/IndicatorTypesFormFields.tsx
@@ -1,40 +1,36 @@
 import i18n from '@dhis2/d2-i18n'
 import React from 'react'
 import {
-    CustomAttributesSection,
     StandardFormField,
     StandardFormSection,
     StandardFormSectionTitle,
     StandardFormSectionDescription,
     NameField,
 } from '../../../components'
-import { SECTIONS_MAP, useSchemaSectionHandleOrThrow } from '../../../lib'
+import { useSchemaSectionHandleOrThrow } from '../../../lib'
 import { FactorField } from './FactorField'
 
 export const IndicatorTypesFormFields = () => {
-    const section = SECTIONS_MAP.indicatorType
     const schemaSection = useSchemaSectionHandleOrThrow()
 
     return (
-        <>
-            <StandardFormSection>
-                <StandardFormSectionTitle>
-                    {i18n.t('Basic information')}
-                </StandardFormSectionTitle>
-                <StandardFormSectionDescription>
-                    {i18n.t(
-                        'Set up the basic information for this Indicator Type.'
-                    )}
-                </StandardFormSectionDescription>
+        <StandardFormSection>
+            <StandardFormSectionTitle>
+                {i18n.t('Basic information')}
+            </StandardFormSectionTitle>
+            <StandardFormSectionDescription>
+                {i18n.t(
+                    'Set up the basic information for this Indicator Type.'
+                )}
+            </StandardFormSectionDescription>
 
-                <StandardFormField>
-                    <NameField schemaSection={schemaSection} />
-                </StandardFormField>
+            <StandardFormField>
+                <NameField schemaSection={schemaSection} />
+            </StandardFormField>
 
-                <StandardFormField>
-                    <FactorField />
-                </StandardFormField>
-            </StandardFormSection>
-        </>
+            <StandardFormField>
+                <FactorField />
+            </StandardFormField>
+        </StandardFormSection>
     )
 }
