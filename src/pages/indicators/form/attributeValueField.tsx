74a14987208cd13d81b5866bd9c365cafdc8b3fe
Chisomchima <chimachisom360@gmail.com>
2025-07-22 15:36:35 +0100

    refactor: update POT dates and simplify attribute value field handling in the indicator form

--- src/pages/indicators/form/attributeValueField.tsx
+++ src/pages/indicators/form/attributeValueField.tsx
@@ -1,89 +1,60 @@
-import { useDataQuery } from '@dhis2/app-runtime'
 import i18n from '@dhis2/d2-i18n'
 import { InputFieldFF, SingleSelectFieldFF } from '@dhis2/ui'
 import React from 'react'
 import { Field } from 'react-final-form'
 import { StandardFormSection, StandardFormField } from '../../../components'
-import { Attribute } from '../../../types/generated'
 
-const ATTRIBUTE_QUERY = {
-    attributes: {
-        resource: 'attributes',
-        params: {
-            fields: 'id,displayFormName,name,valueType,mandatory,optionSet[options[code,displayName,id]]',
-        },
+const ATTRIBUTES = [
+    {
+        id: 'Z4X3J7jMLYV',
+        label: 'Classification',
+        type: 'select',
+        options: [
+            { code: 'INPUT', displayName: 'Input' },
+            { code: 'ACTIVITY', displayName: 'Activity' },
+            { code: 'OUTPUT', displayName: 'Output' },
+            { code: 'IMPACT', displayName: 'Impact' },
+        ],
     },
-}
+    {
+        id: 'dLHLR5O4YFI',
+        label: 'PEPFAR ID',
+        type: 'text',
+    },
+] as const
 
-type AttributeQueryResponse = {
-    attributes: Attribute[]
-}
-
-export const AttributeValueField = () => {
-    const { data, loading } = useDataQuery<{
-        attributes: AttributeQueryResponse
-    }>(ATTRIBUTE_QUERY)
-
-    const allAttributes = data?.attributes?.attributes ?? []
-
-    const pepfarAttr = allAttributes.find(
-        (attr: { displayFormName: string }) =>
-            attr.displayFormName === 'PEPFAR ID'
-    )
-    const classAttr = allAttributes.find(
-        (attr: { displayFormName: string }) =>
-            attr.displayFormName === 'Classification'
-    )
-
-    const createHiddenAttributeField = (index: number, attr: Attribute) => (
-        <>
-            <Field
-                name={`attributeValues[${index}].attribute`}
-                initialValue={{
-                    id: attr.id,
-                    name: attr.name,
-                    displayName: attr.displayFormName,
-                }}
-                component="input"
-                type="hidden"
-            />
-        </>
-    )
-
-    return (
-        <StandardFormSection>
-            {pepfarAttr && (
-                <StandardFormField>
+export const AttributeValueField = () => (
+    <StandardFormSection>
+        {ATTRIBUTES.map((attr, i) => (
+            <StandardFormField key={attr.id}>
+                {attr.type === 'select' ? (
                     <Field
-                        name="attributeValues[0].value"
-                        component={InputFieldFF}
-                        label={pepfarAttr.displayFormName}
-                        inputWidth="400px"
-                        loading={loading}
-                    />
-                    {createHiddenAttributeField(0, pepfarAttr)}
-                </StandardFormField>
-            )}
-
-            {classAttr && classAttr.optionSet?.options?.length > 0 && (
-                <StandardFormField>
-                    <Field
-                        name="attributeValues[1].value"
+                        name={`attributeValues[${i}].value`}
                         component={SingleSelectFieldFF}
-                        label={classAttr.displayFormName}
+                        label={attr.label}
                         inputWidth="400px"
-                        options={classAttr.optionSet.options.map(
-                            (opt: { displayName: string; code: string }) => ({
-                                label: opt.displayName,
-                                value: opt.code,
-                            })
-                        )}
+                        options={attr.options.map((opt) => ({
+                            label: opt.displayName,
+                            value: opt.code,
+                        }))}
                         placeholder={i18n.t('Select option')}
-                        loading={loading}
                     />
-                    {createHiddenAttributeField(1, classAttr)}
-                </StandardFormField>
-            )}
-        </StandardFormSection>
-    )
-}
+                ) : (
+                    <Field
+                        name={`attributeValues[${i}].value`}
+                        component={InputFieldFF}
+                        label={attr.label}
+                        inputWidth="400px"
+                    />
+                )}
+
+                <Field
+                    name={`attributeValues[${i}].attribute`}
+                    component="input"
+                    type="hidden"
+                    initialValue={{ id: attr.id }}
+                />
+            </StandardFormField>
+        ))}
+    </StandardFormSection>
+)
