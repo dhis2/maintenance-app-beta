e99b6d833b1f2ce8a7422bfdfef1c87981af835f
Flaminia Cavallo <flaminia@dhis2.org>
2025-07-29 06:58:00 -0700

    feat: wrte test that produces the files needede

---
+++ src/contracts/generateContracts.spec.ts
@@ -0,0 +1,42 @@
+import { writeFileSync, mkdirSync } from 'fs'
+import { z, ZodSchema } from 'zod'
+import zodToJsonSchema from 'zod-to-json-schema'
+import { categoryListSchema } from '../pages/categories/form'
+
+const generateContract = ({
+    method,
+    path,
+    name,
+    expectedSchema,
+}: {
+    method: string
+    path: string
+    name: string
+    expectedSchema: ZodSchema<any>
+}) => {
+    const contractPath = `contracts/${name}/contract.json`
+    const schemaPath = `contracts/${name}/json-schema.json`
+    const request = {
+        name,
+        httpMethod: method,
+        requestUrl: path,
+        responseStatus: 200,
+        jsonSchema: schemaPath,
+    }
+    const schema = zodToJsonSchema(expectedSchema, name)
+    mkdirSync(`contracts/${name}`, { recursive: true })
+    writeFileSync(contractPath, JSON.stringify(request))
+    writeFileSync(schemaPath, JSON.stringify(schema.definitions))
+}
+
+describe('contracts', () => {
+    it('should generate all contracts', () => {
+        generateContract({
+            method: 'GET',
+            path: 'categories',
+            name: 'categories',
+            expectedSchema: z.array(categoryListSchema),
+        })
+        // expect(1+1).toBe(2)
+    })
+})
