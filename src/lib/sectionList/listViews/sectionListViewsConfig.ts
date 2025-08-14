3aab74861b04111f887696cf018c5ff492cf5cdc
Chisom Chima <87203527+Chisomchima@users.noreply.github.com>
2025-08-14 18:27:53 +0100

    feat: add option group list (#614)
    * feat: enhance DefaultSectionedFormFooter with loading and dirty state handling
    
    * feat: enhance DefaultSectionedFormFooter with loading and dirty state handling
    
    * feat: add option group list
    
    * feat: option group schema and test
    
    * chore: cleanup available columns

--- src/lib/sectionList/listViews/sectionListViewsConfig.ts
+++ src/lib/sectionList/listViews/sectionListViewsConfig.ts
@@ -273,6 +273,26 @@ export const modelListViewsConfig = {
             default: ['dataDimensionType', 'categoryCombo'],
         },
     },
+    optionGroup: {
+        columns: {
+            available: [
+                DESCRIPTORS.shortName,
+                'description',
+                'favorite',
+                { label: i18n.t('Owner'), path: 'user.displayName' },
+            ],
+            default: [
+                DESCRIPTORS.name,
+                DESCRIPTORS.publicAccess,
+                'lastUpdated',
+            ],
+        },
+        filters: {
+            default: [],
+            available: [],
+            overrideDefaultAvailable: true,
+        },
+    },
     indicator: {
         columns: {
             default: [
