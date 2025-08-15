816c3fe11cf303da205c81427405be56cb7bc7a0
Thomas Zemp <tom@dhis2.org>
2025-08-15 13:20:38 +0200

    fix: remove unused import

--- src/pages/overview/Other.tsx
+++ src/pages/overview/Other.tsx
@@ -2,7 +2,7 @@ import i18n from '@dhis2/d2-i18n'
 import React from 'react'
 import { OVERVIEW_SECTIONS, SECTIONS_MAP } from '../../lib'
 import { SummaryCard, SummaryCardGroup } from './card'
-import { OverviewGroup, OverviewGroupSummary } from './group'
+import { OverviewGroup } from './group'
 
 const TITLE = OVERVIEW_SECTIONS.other.titlePlural
 
