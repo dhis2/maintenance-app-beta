48c942a4d696e89a4c4dac8b9af92362ff0a2285
Flaminia <fl.cavallo@gmail.com>
2025-08-25 05:33:07 -0700

    feat: sections transfers fields (#613) [skip release]
    * feat: transfer filter by cat combo and manage grey fields
    
    * refactor: small changes for readability
    
    * fix: cat combo filter in sections data element to contain all of the data set category combos
    
    * fix: in greyed fields, filter DE by the selected cat combo

--- src/pages/dataSetsWip/form/dataEntryForm/sectionForm/DataSetSectionForm.tsx
+++ src/pages/dataSetsWip/form/dataEntryForm/sectionForm/DataSetSectionForm.tsx
@@ -1,5 +1,6 @@
 import i18n from '@dhis2/d2-i18n'
 import { useQuery } from '@tanstack/react-query'
+import arrayMutators from 'final-form-arrays'
 import React, { useCallback, useMemo } from 'react'
 import { useParams } from 'react-router-dom'
 import { FormBase, FormBaseProps } from '../../../../../components'
@@ -32,7 +33,8 @@ export const fieldFilters = [
     'showColumnTotals',
     'disableDataElementAutoGroup',
     'displayOptions',
-    'dataElements[id,displayName]',
+    'dataElements[id,displayName,categoryCombo[id]]',
+    'greyedFields[dataElement, categoryOptionCombo]',
 ] as const
 
 const dataSetSectionSchemaSection = {
@@ -48,7 +50,7 @@ export type SectionFormValues = PickWithFieldFilters<
     typeof fieldFilters
 > & {
     dataSet: { id: string }
-    displayOptions?: undefined
+    displayOptions?: string
 }
 type PartialSectionFormValues = Partial<SectionFormValues>
 type SubmittedSectionFormValues = PartialSectionFormValues & DisplayableModel
@@ -99,6 +101,7 @@ export const DataSetSectionForm = ({
             onSubmit={onSubmit}
             valueFormatter={valueFormatter}
             includeAttributes={false}
+            mutators={{ ...arrayMutators }}
         >
             <DataSetSectionFormContents onCancel={onCancel} />
         </FormBase>
