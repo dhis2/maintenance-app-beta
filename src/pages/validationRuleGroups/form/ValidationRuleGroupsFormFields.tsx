30df23f3f0e30c04ce254e82f14d587e283fd048
Milagros <milagros@dhis2.org>
2025-09-26 13:41:01 +0200

    feat: add orphan filter

--- src/pages/validationRuleGroups/form/ValidationRuleGroupsFormFields.tsx
+++ src/pages/validationRuleGroups/form/ValidationRuleGroupsFormFields.tsx
@@ -16,48 +16,62 @@ function ValidationRuleGroupsFormFields() {
     const schemaSection = useSchemaSectionHandleOrThrow()
 
     return (
-        <StandardFormSection>
-            <StandardFormSectionTitle>
-                {i18n.t('Basic information')}
-            </StandardFormSectionTitle>
-            <StandardFormSectionDescription>
-                {i18n.t(
-                    'Set up the basic information for this validation rule group.'
-                )}
-            </StandardFormSectionDescription>
+        <>
+            <StandardFormSection>
+                <StandardFormSectionTitle>
+                    {i18n.t('Basic information')}
+                </StandardFormSectionTitle>
+                <StandardFormSectionDescription>
+                    {i18n.t(
+                        'Set up the basic information for this validation rule group.'
+                    )}
+                </StandardFormSectionDescription>
 
-            <StandardFormField>
-                <NameField schemaSection={schemaSection} />
-            </StandardFormField>
-            <StandardFormField>
-                <CodeField schemaSection={schemaSection} />
-            </StandardFormField>
-            <StandardFormField>
-                <DescriptionField
-                    helpText={i18n.t(
-                        'Explain the purpose of this validation rule group.'
-                    )}
-                />
-            </StandardFormField>
+                <StandardFormField>
+                    <NameField schemaSection={schemaSection} />
+                </StandardFormField>
+                <StandardFormField>
+                    <CodeField schemaSection={schemaSection} />
+                </StandardFormField>
+                <StandardFormField>
+                    <DescriptionField
+                        helpText={i18n.t(
+                            'Explain the purpose of this validation rule group.'
+                        )}
+                    />
+                </StandardFormField>
+            </StandardFormSection>
 
-            <StandardFormField>
-                <ModelTransferField
-                    dataTest="validationRules-transfer"
-                    name="validationRules"
-                    query={{
-                        resource: 'validationRules',
-                    }}
-                    leftHeader={i18n.t('Available validation rules')}
-                    rightHeader={i18n.t('Selected validation rules')}
-                    filterPlaceholder={i18n.t(
-                        'Filter available validation rules'
+            <StandardFormSection>
+                <StandardFormSectionTitle>
+                    {i18n.t('Validation Rules')}
+                </StandardFormSectionTitle>
+                <StandardFormSectionDescription>
+                    {i18n.t(
+                        'Choose the validation rules to include in this validation rule group.'
                     )}
-                    filterPlaceholderPicked={i18n.t(
-                        'Filter selected validation rules'
-                    )}
-                />
-            </StandardFormField>
-        </StandardFormSection>
+                </StandardFormSectionDescription>
+
+                <StandardFormField>
+                    <ModelTransferField
+                        dataTest="validationRules-transfer"
+                        name="validationRules"
+                        query={{
+                            resource: 'validationRules',
+                        }}
+                        filterUnassignedTo={'validationRuleGroups'}
+                        leftHeader={i18n.t('Available validation rules')}
+                        rightHeader={i18n.t('Selected validation rules')}
+                        filterPlaceholder={i18n.t(
+                            'Filter available validation rules'
+                        )}
+                        filterPlaceholderPicked={i18n.t(
+                            'Filter selected validation rules'
+                        )}
+                    />
+                </StandardFormField>
+            </StandardFormSection>
+        </>
     )
 }
 
