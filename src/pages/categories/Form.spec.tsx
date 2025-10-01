dbcae4b7c8890c2ff5d176853a7b0609de3f459b
Milagros <milagros@dhis2.org>
2025-10-01 14:05:22 +0200

    feat: test category

--- src/pages/categories/Form.spec.tsx
+++ src/pages/categories/Form.spec.tsx
@@ -1,23 +1,222 @@
+import { faker } from '@faker-js/faker'
+import { render } from '@testing-library/react'
+import React from 'react'
+import schemaMock from '../../__mocks__/schema/categoriesSchema.json'
+import { FOOTER_ID } from '../../app/layout/Layout'
+import { SECTIONS_MAP } from '../../lib'
+import {
+    randomLongString,
+    testCategoryForm,
+    testCategoryOption,
+} from '../../testUtils/builders'
+import { generateRenderer } from '../../testUtils/generateRenderer'
+import TestComponentWithRouter from '../../testUtils/TestComponentWithRouter'
+import { uiActions } from '../../testUtils/uiActions'
+import { uiAssertions } from '../../testUtils/uiAssertions'
+import { Component as Edit } from './Edit'
+import { Component as New } from './New'
+import resetAllMocks = jest.resetAllMocks
+
+const section = SECTIONS_MAP.category
+const mockSchema = schemaMock
+jest.mock('use-debounce', () => ({
+    useDebouncedCallback: (fn: any) => fn,
+}))
+
 describe('Categories form tests', () => {
-    describe('Common', () => {
-        it('should not submit when a required values is missing ', () => {})
-        it('should show an error if name field is too long', () => {})
-        it('should show an error if short name field is too long', () => {})
-        it('should show an error if code field is too long', () => {})
-        it('should show an error if description field is too long', () => {})
-        it('should show an error if name field is a duplicate', () => {})
-        it('should show an error if short name field is a duplicate', () => {})
-        it('should show an error if code field is a duplicate', () => {})
-        it('not show an add all button for category options', () => {})
+    const createMock = jest.fn()
+    const updateMock = jest.fn()
+
+    beforeEach(() => {
+        resetAllMocks()
+        const portalRoot = document.createElement('div')
+        portalRoot.setAttribute('id', FOOTER_ID)
+        document.body.appendChild(portalRoot)
     })
-    describe('New', () => {
+
+    afterEach(() => {
+        const portalRoot = document.getElementById(FOOTER_ID)
+        if (portalRoot) {
+            portalRoot.remove()
+        }
+    })
+
+    describe('Common', () => {
+        const renderForm = generateRenderer(
+            { section, mockSchema },
+            (
+                routeOptions,
+                { matchingExistingElementFilter = undefined } = {}
+            ) => {
+                const categoryOptions = [
+                    testCategoryOption(),
+                    testCategoryOption(),
+                ]
+
+                const screen = render(
+                    <TestComponentWithRouter
+                        path={`/${section.namePlural}`}
+                        customData={{
+                            categoryOptions: () => ({ categoryOptions }),
+                            categories: (type: any, params: any) => {
+                                if (type === 'create') {
+                                    createMock(params)
+                                    return { statusCode: 204 }
+                                }
+                                if (type === 'read') {
+                                    if (
+                                        params?.params?.filter?.includes(
+                                            matchingExistingElementFilter
+                                        )
+                                    ) {
+                                        return {
+                                            pager: { total: 1 },
+                                            categories: [testCategoryForm()],
+                                        }
+                                    }
+                                    return {
+                                        pager: { total: 0 },
+                                        categories: [],
+                                    }
+                                }
+                            },
+                        }}
+                        routeOptions={routeOptions}
+                    >
+                        <New />
+                    </TestComponentWithRouter>
+                )
+
+                return { screen }
+            }
+        )
+
+        it('should not submit when a required values is missing ', async () => {
+            const { screen } = await renderForm()
+            await uiActions.submitForm(screen)
+            expect(createMock).not.toHaveBeenCalled()
+            uiAssertions.expectFieldToHaveError(
+                'formfields-name',
+                'Required',
+                screen
+            )
+            uiAssertions.expectFieldToHaveError(
+                'formfields-shortName',
+                'Required',
+                screen
+            )
+        })
+
+        it('should show an error if name field is too long', async () => {
+            const { screen } = await renderForm()
+            const longName = randomLongString(231)
+            await uiActions.enterName(longName, screen)
+            await uiAssertions.expectNameToErrorWhenExceedsLength(screen)
+            await uiActions.submitForm(screen)
+            expect(createMock).not.toHaveBeenCalled()
+        })
+
+        it('should show an error if short name field is too long', async () => {
+            const { screen } = await renderForm()
+            const longName = randomLongString(54)
+            await uiActions.enterInputFieldValue('shortName', longName, screen)
+            await uiAssertions.expectInputToErrorWhenExceedsLength(
+                'shortName',
+                50,
+                screen
+            )
+            await uiActions.submitForm(screen)
+            expect(createMock).not.toHaveBeenCalled()
+        })
+
+        it('should show an error if code field is too long', async () => {
+            const { screen } = await renderForm()
+            const longText = randomLongString(57)
+            await uiActions.enterInputFieldValue('code', longText, screen)
+            await uiAssertions.expectInputToErrorWhenExceedsLength(
+                'code',
+                50,
+                screen
+            )
+            await uiActions.submitForm(screen)
+            expect(createMock).not.toHaveBeenCalled()
+        })
+
+        it('should show an error if description field is too long', async () => {
+            const { screen } = await renderForm()
+            const longText = randomLongString(2010)
+            await uiActions.enterInputFieldValue(
+                'description',
+                longText,
+                screen
+            )
+            await uiAssertions.expectInputToErrorWhenExceedsLength(
+                'description',
+                2000,
+                screen
+            )
+            await uiActions.submitForm(screen)
+            expect(createMock).not.toHaveBeenCalled()
+        })
+
+        it('should show an error if name field is a duplicate', async () => {
+            const existingName = faker.company.name()
+            const { screen } = await renderForm({
+                matchingExistingElementFilter: `name:ieq:${existingName}`,
+            })
+            await uiAssertions.expectNameToErrorWhenDuplicate(
+                existingName,
+                screen
+            )
+            await uiActions.submitForm(screen)
+            expect(createMock).not.toHaveBeenCalled()
+        })
+
+        it('should show an error if short name field is a duplicate', async () => {
+            const existingShortName = faker.company.name()
+            const { screen } = await renderForm({
+                matchingExistingElementFilter: `shortName:ieq:${existingShortName}`,
+            })
+            await uiAssertions.expectInputToErrorWhenDuplicate(
+                'shortName',
+                existingShortName,
+                screen
+            )
+            await uiActions.submitForm(screen)
+            expect(createMock).not.toHaveBeenCalled()
+        })
+
+        it('should show an error if code field is a duplicate', async () => {
+            const existingCode = faker.science.chemicalElement().symbol
+            const { screen } = await renderForm({
+                matchingExistingElementFilter: `code:ieq:${existingCode}`,
+            })
+            await uiAssertions.expectCodeToErrorWhenDuplicate(
+                existingCode,
+                screen
+            )
+            await uiActions.submitForm(screen)
+            expect(createMock).not.toHaveBeenCalled()
+        })
+
+        it('not show an add all button for category options', async () => {
+            const { screen } = await renderForm()
+            const addAllButton = screen.getByTestId(
+                'legendset-transfer-actions-addall'
+            )
+            expect(addAllButton).not.toBeVisible()
+        })
+    })
+
+    /*describe('New', () => {
         it('contain all needed field', () => {})
         it('should have a cancel button with a link back to the list view', () => {})
         it('should submit the data', () => {})
     })
+
     describe('Edit', () => {
         it('contain all needed field prefilled', () => {})
         it('should submit the data and return to the list view on success when a field is changed', () => {})
         it('should do nothing and return to the list view on success when no field is changed', () => {})
-    })
+    })*/
 })
