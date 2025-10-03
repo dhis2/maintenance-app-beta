e47284c8d66175bd5fada7fd8d3ac79695eb2795
Thomas Zemp <tom@dhis2.org>
2025-10-03 15:22:17 +0200

    fix: warn when changing data element value type [DHIS2-18151] (#639)

--- src/pages/dataElements/fields/OptionSetField.tsx
+++ src/pages/dataElements/fields/OptionSetField.tsx
@@ -1,22 +1,35 @@
 import i18n from '@dhis2/d2-i18n'
 import React from 'react'
+import { useField } from 'react-final-form'
 import { useHref } from 'react-router'
-import { EditableFieldWrapper } from '../../../components'
+import {
+    EditableFieldWrapper,
+    ConfirmationModalWrapper,
+} from '../../../components'
 import {
     ModelSingleSelectFormField,
     useRefreshModelSingleSelect,
 } from '../../../components/metadataFormControls/ModelSingleSelect'
+import { getConstantTranslation } from '../../../lib'
 
 export function OptionSetField() {
     const newOptionSetLink = useHref('/optionSets/new')
     const refresh = useRefreshModelSingleSelect({ resource: 'optionSets' })
+    const { input } = useField('optionSet')
+    const { input: valueTypeInput } = useField('valueType')
 
-    return (
+    const renderComponent = ({
+        onChange,
+    }: {
+        onChange: (event: any) => void
+    }) => (
         <EditableFieldWrapper
             onRefresh={() => refresh()}
             onAddNew={() => window.open(newOptionSetLink, '_blank')}
         >
             <ModelSingleSelectFormField
+                fullyOverrideOnChange={true}
+                onChange={onChange}
                 showNoValueOption
                 inputWidth="400px"
                 dataTest="formfields-optionset"
@@ -34,5 +47,39 @@ export function OptionSetField() {
                 )}
             />
         </EditableFieldWrapper>
+    )
+
+    return (
+        <ConfirmationModalWrapper
+            onChange={(selected) => {
+                input.onChange(selected)
+                input.onBlur()
+            }}
+            renderComponent={renderComponent}
+            skipConfirmationLogic={(selection) => {
+                return (
+                    !selection || selection?.valueType === valueTypeInput?.value
+                )
+            }}
+            modalTitle={i18n.t('Option set update will change value type')}
+            modalMessage={i18n.t(
+                'Updating the option set will change the value type which may cause problems when generating analytics tables if there is existing data for this data element.'
+            )}
+            modalMessageSelectionSpecificConfirmation={(selection) =>
+                i18n.t(
+                    'Are you sure you want to change the option set to {{optionSet}} which will change the value type from {{currentValueType}} to {{newValueType}}?',
+                    {
+                        optionSet:
+                            selection?.displayName ?? i18n.t('this option set'),
+                        currentValueType: getConstantTranslation(
+                            valueTypeInput.value
+                        ),
+                        newValueType: getConstantTranslation(
+                            selection.valueType
+                        ),
+                    }
+                )
+            }
+        />
     )
 }
