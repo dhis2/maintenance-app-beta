6697be5b6df62b0b0199685f4d78ec57e4aa242c
Chisom Chima <87203527+Chisomchima@users.noreply.github.com>
2025-10-22 09:50:25 +0100

    feat: add data approval level components and schema (#661)
    * feat: add data approval level components and schema
    
    * feat: add 'href' to available model list view columns

--- src/testUtils/builders.ts
+++ src/testUtils/builders.ts
@@ -17,6 +17,7 @@ import { categoryOptionGroupListSchema } from '../pages/categoryOptionGroups/for
 import { categoryOptionGroupSetListSchema } from '../pages/categoryOptionGroupSets/form/categoryOptionGroupSetSchema'
 import { categoryOptionListSchema } from '../pages/categoryOptions/form/categoryOptionSchema'
 import { ConstantsListSchema } from '../pages/constants/form/ConstantFormSchema'
+import { dataApprovalLevelListSchema } from '../pages/dataApprovalLevels/form/dataApprovalLevelsSchema'
 import { dataApprovalWorkflowListSchema } from '../pages/dataApprovalWorkflows/form/dataApprovalWorkflowSchema'
 import { dataElementGroupListSchema } from '../pages/dataElementGroups/form/dataElementGroupSchema'
 import { dataElementGroupSetSchema } from '../pages/dataElementGroupSets/form'
@@ -282,6 +283,13 @@ export const testProgramIndicator = (overwrites: Record<any, any> = {}) => ({
 
 export const testAttributeList = (overwrites: Record<any, any> = {}) => ({
     ...generateMock(attributeListSchema, { mockeryMapper }),
+    ...overwrites,
+})
+
+export const testdataApprovalLevelList = (
+    overwrites: Record<any, any> = {}
+) => ({
+    ...generateMock(dataApprovalLevelListSchema, { mockeryMapper }),
     ...overwrites,
 })
 
