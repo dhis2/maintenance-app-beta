a3ecc2d80d23e752fc98fe05140cdac34012d767
henrikmv <henrik@devOtta.no>
2026-01-30 10:01:53 +0100

    feat: add titile for padded container

--- src/pages/relationshipTypes/form/RelationshipSidesFields.tsx
+++ src/pages/relationshipTypes/form/RelationshipSidesFields.tsx
@@ -1,6 +1,5 @@
 import i18n from '@dhis2/d2-i18n'
 import React from 'react'
-import { StandardFormSubsectionTitle } from '../../../components'
 import { PaddedContainer } from '../../../components/ExpressionBuilder/PaddedContainer'
 import {
     ConstraintField,
@@ -16,15 +15,13 @@ export const RelationshipSideFields = ({
     prefix,
 }: RelationshipSideFieldsProps) => {
     return (
-        <PaddedContainer>
-            <StandardFormSubsectionTitle>
-                {i18n.t(
-                    prefix === 'from'
-                        ? 'Initiating side (From)'
-                        : 'Receiving side (To)'
-                )}
-            </StandardFormSubsectionTitle>
-
+        <PaddedContainer
+            title={i18n.t(
+                prefix === 'from'
+                    ? 'Initiating side (From)'
+                    : 'Receiving side (To)'
+            )}
+        >
             <ConstraintField prefix={prefix} />
             <div style={{ display: 'flex', flexWrap: 'wrap', gap: 16 }}>
                 <TrackedEntityTypeField prefix={prefix} />
