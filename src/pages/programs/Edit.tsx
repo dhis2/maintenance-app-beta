5ac86876f0800b1d44616d77ed5910647e622832
Flaminia Cavallo <flaminia@dhis2.org>
2026-03-04 15:56:11 +0100

    feat: hide in progress event programs

--- src/pages/programs/Edit.tsx
+++ src/pages/programs/Edit.tsx
@@ -1,9 +1,9 @@
 import { useQuery } from '@tanstack/react-query'
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { useQueryParam } from 'use-query-params'
 import { LegacyAppRedirect } from '../../app/routes/LegacyAppRedirect'
 import { SECTIONS_MAP, useBoundResourceQueryFn } from '../../lib'
-import { PickWithFieldFilters, Program } from '../../types/generated'
 import { EditEventProgram } from './EditEventProgram'
 import { EditTrackerProgram } from './EditTrackerProgram'
 
@@ -15,6 +15,7 @@ type ProgramValues = {
 export const Component = () => {
     const queryFn = useBoundResourceQueryFn()
     const modelId = useParams().id as string
+    const [inDevMode] = useQueryParam('dev')
 
     const program = useQuery({
         queryFn: queryFn<ProgramValues>,
@@ -29,8 +30,15 @@ export const Component = () => {
         ] as const,
     })
 
-    if (program?.data?.programType === 'WITHOUT_REGISTRATION') {
+    if (
+        program?.data?.programType === 'WITHOUT_REGISTRATION' &&
+        inDevMode === 'yes'
+    ) {
         return <EditEventProgram />
+    }
+
+    if (program?.data?.programType === 'WITHOUT_REGISTRATION') {
+        return <LegacyAppRedirect section={SECTIONS_MAP.program} />
     }
 
     return <EditTrackerProgram />
