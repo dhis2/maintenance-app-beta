991bcc6f678c832449f3e6f2ea0788aaf9cf0c62
Joseph John Aas Cooper <33054985+cooper-joe@users.noreply.github.com>
2026-03-10 09:42:33 +0100

    fix(program-indicator-group): transfer all functionality

--- src/pages/programIndicatorGroups/form/ProgramIndicatorGroupsFormFields.tsx
+++ src/pages/programIndicatorGroups/form/ProgramIndicatorGroupsFormFields.tsx
@@ -60,6 +60,7 @@ export const ProgramIndicatorGroupsFormFields = () => {
                             'Search selected program indicators'
                         )}
                         dataTest="program-indicators-transfer"
+                        maxSelections={Infinity}
                     />
                 </StandardFormField>
             </StandardFormSection>
