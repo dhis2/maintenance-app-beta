77821e6184167416fed93c727a8904dbc9c368b3
Thomas Zemp <tom@dhis2.org>
2026-03-16 09:24:28 +0100

    feat: rename app [DHIS2-21090] (#804)

--- d2.config.js
+++ d2.config.js
@@ -1,12 +1,194 @@
 const config = {
     id: '3c6d0723-904c-4c7a-bbd6-35f3c3aa356b',
     type: 'app',
-    title: 'Maintenance (Preview)',
-    name: 'maintenance-preview',
+    title: 'Metadata Management',
+    name: 'metadata-management',
     entryPoints: {
         app: './src/App.tsx',
     },
     minDHIS2Version: '2.41',
+    shortcuts: [
+        {
+            name: 'Categories',
+            url: '#/categories',
+        },
+        {
+            name: 'Category options',
+            url: '#/categoryOptions',
+        },
+        {
+            name: 'Category combinations',
+            url: '#/categoryCombos',
+        },
+        {
+            name: 'Category option combinations',
+            url: '#/categoryOptionCombos',
+        },
+        {
+            name: 'Category option groups',
+            url: '#/categoryOptionGroups',
+        },
+        {
+            name: 'Category option group sets',
+            url: '#/categoryOptionGroupSets',
+        },
+        {
+            name: 'Data elements',
+            url: '#/dataElements',
+        },
+        {
+            name: 'Data element groups',
+            url: '#/dataElementGroups',
+        },
+        {
+            name: 'Data element group sets',
+            url: '#/dataElementGroupSets',
+        },
+        {
+            name: 'Data sets',
+            url: '#/dataSets',
+        },
+        {
+            name: 'Data set notifications',
+            url: '#/dataSetNotificationTemplates',
+        },
+        {
+            name: 'Indicators',
+            url: '#/indicators',
+        },
+        {
+            name: 'Indicator types',
+            url: '#/indicatorTypes',
+        },
+        {
+            name: 'Indicator groups',
+            url: '#/indicatorGroups',
+        },
+        {
+            name: 'Indicator group sets',
+            url: '#/indicatorGroupSets',
+        },
+        {
+            name: 'Program indicators',
+            url: '#/programIndicators',
+        },
+        {
+            name: 'Program indicator groups',
+            url: '#/programIndicatorGroups',
+        },
+        {
+            name: 'Organisation units',
+            url: '#/organisationUnits',
+        },
+        {
+            name: 'Organisation unit groups',
+            url: '#/organisationUnitGroups',
+        },
+        {
+            name: 'Organisation unit group sets',
+            url: '#/organisationUnitGroupSets',
+        },
+        {
+            name: 'Organisation unit levels',
+            url: '#/organisationUnitLevels',
+        },
+        {
+            name: 'Programs',
+            url: '#/programs',
+        },
+        {
+            name: 'Tracked entity attributes',
+            url: '#/trackedEntityAttributes',
+        },
+        {
+            name: 'Relationship types',
+            url: '#/relationshipTypes',
+        },
+        {
+            name: 'Tracked entity types',
+            url: '#/trackedEntityTypes',
+        },
+        {
+            name: 'Program rules',
+            url: '#/programRules',
+        },
+        {
+            name: 'Program rule variables',
+            url: '#programRuleVariables',
+        },
+        {
+            name: 'Validation rules',
+            url: '#/validationRules',
+        },
+        {
+            name: 'Validation rule groups',
+            url: '#/validationRuleGroups',
+        },
+        {
+            name: 'Validation notifications',
+            url: '#/validationNotificationTemplates',
+        },
+        {
+            name: 'Constants',
+            url: '#/constants',
+        },
+        {
+            name: 'Attributes',
+            url: '#/attributes',
+        },
+        {
+            name: 'Option sets',
+            url: '#/optionSets',
+        },
+        {
+            name: 'Option groups',
+            url: '#/optionGroups',
+        },
+        {
+            name: 'Option group sets',
+            url: '#/optionGroupSets',
+        },
+        {
+            name: 'Legends',
+            url: '#/legendSets',
+        },
+        {
+            name: 'Predictors',
+            url: '#/predictors',
+        },
+        {
+            name: 'Predictor groups',
+            url: '#/predictorGroups',
+        },
+        {
+            name: 'External map layers',
+            url: '#/externalMapLayers',
+        },
+        {
+            name: 'Data approval levels',
+            url: '#/dataApprovalLevels',
+        },
+        {
+            name: 'Data approval workflows',
+            url: '#/dataApprovalWorkflows',
+        },
+        {
+            name: 'Locales',
+            url: '#/locales',
+        },
+        {
+            name: 'SQL views',
+            url: '#/sqlViews',
+        },
+        {
+            name: 'Icons',
+            url: '#/icons',
+        },
+        {
+            name: 'Analytics table hooks',
+            url: '#/analyticsTableHooks',
+        },
+    ],
 }
 
 module.exports = config
