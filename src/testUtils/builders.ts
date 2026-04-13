08996bbc154e36073018974dfa4236f6bfa408b5
henrikmv <henrik@devOtta.no>
2026-04-13 17:36:53 +0200

    test: add  tests for data approval level form

--- src/testUtils/builders.ts
+++ src/testUtils/builders.ts
@@ -29,7 +29,10 @@ import {
 } from '../pages/categoryOptionGroupSets/form/categoryOptionGroupSetSchema'
 import { categoryOptionListSchema } from '../pages/categoryOptions/form/categoryOptionSchema'
 import { ConstantsListSchema } from '../pages/constants/form/ConstantFormSchema'
-import { dataApprovalLevelListSchema } from '../pages/dataApprovalLevels/form/dataApprovalLevelsSchema'
+import {
+    dataApprovalLevelFormSchema,
+    dataApprovalLevelListSchema,
+} from '../pages/dataApprovalLevels/form/dataApprovalLevelsSchema'
 import { dataApprovalWorkflowListSchema } from '../pages/dataApprovalWorkflows/form/dataApprovalWorkflowSchema'
 import { dataElementGroupListSchema } from '../pages/dataElementGroups/form/dataElementGroupSchema'
 import { dataElementGroupSetSchema } from '../pages/dataElementGroupSets/form'
@@ -369,6 +372,13 @@ export const testdataApprovalLevelList = (
     overwrites: Record<any, any> = {}
 ) => ({
     ...generateMock(dataApprovalLevelListSchema, { stringMap }),
+    ...overwrites,
+})
+
+export const testDataApprovalLevelForm = (
+    overwrites: Record<any, any> = {}
+) => ({
+    ...generateMock(dataApprovalLevelFormSchema, { stringMap }),
     ...overwrites,
 })
 
