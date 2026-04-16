c5bece15b724a170ffe8ed1144fde6d8e6d1c283
henrikmv <henrik@devOtta.no>
2026-04-16 17:44:15 +0200

    feat: add clearable option to CategoryOptionGroupSetField

--- src/pages/dataApprovalLevels/form/fields/CategoryOptionGroupSetField.tsx
+++ src/pages/dataApprovalLevels/form/fields/CategoryOptionGroupSetField.tsx
@@ -16,6 +16,7 @@ export function CategoryOptionGroupSetField() {
                 },
             }}
             refreshResource="categoryOptionGroupSets"
+            clearable
         />
     )
 }
