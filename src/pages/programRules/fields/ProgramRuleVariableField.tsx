16307fab6dbe3d823a44b27fae9f52f53ec2f496
Flaminia Cavallo <flaminia@dhis2.org>
2026-04-21 10:06:05 +0200

    fix: small review fix

--- src/pages/programRules/fields/ProgramRuleVariableField.tsx
+++ src/pages/programRules/fields/ProgramRuleVariableField.tsx
@@ -65,7 +65,7 @@ export function ProgramRuleVariableField({
                       ({
                           id: '',
                           name,
-                          displayName: undefined,
+                          displayName: name,
                       } as ProgramRuleVariableModel)
                     : undefined
 
