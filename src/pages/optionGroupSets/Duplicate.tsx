e25cf5f6c26ecc9cf942e4a5d60b24021c70585f
Thomas Zemp <tom@dhis2.org>
2026-04-29 10:40:48 +0200

    feat: add option cloning [DHIS2-21230]

---
+++ src/pages/optionGroupSets/Duplicate.tsx
@@ -0,0 +1,62 @@
+import { useQuery } from '@tanstack/react-query'
+import { omit } from 'lodash'
+import React from 'react'
+import { useSearchParams } from 'react-router-dom'
+import { FormBase } from '../../components'
+import { DefaultDuplicateFormContents } from '../../components/form/DefaultFormContents'
+import { DEFAULT_FIELD_FILTERS, SECTIONS_MAP, useOnSubmitNew } from '../../lib'
+import { useBoundResourceQueryFn } from '../../lib/query/useBoundQueryFn'
+import { PickWithFieldFilters } from '../../types/generated'
+import { OptionGroupSet } from '../../types/models'
+import OptionGroupSetFormFields from './form/OptionGroupSetFormFields'
+import { validate } from './form/optionGroupSetSchema'
+
+const fieldFilters = [
+    ...DEFAULT_FIELD_FILTERS,
+    'name',
+    'shortName',
+    'code',
+    'description',
+    'dataDimensions',
+    'optionSet[id,displayName]',
+    'optionGroups[id,displayName]',
+] as const
+
+export type OptionGroupSetFormValues = PickWithFieldFilters<
+    OptionGroupSet,
+    typeof fieldFilters
+>
+
+const section = SECTIONS_MAP.optionGroupSet
+
+export const Component = () => {
+    const queryFn = useBoundResourceQueryFn()
+    const [searchParams] = useSearchParams()
+    const duplicatedModelId = searchParams.get('duplicatedId') as string
+
+    const query = {
+        resource: 'optionGroupSets',
+        id: duplicatedModelId,
+        params: {
+            fields: fieldFilters.concat(),
+        },
+    }
+    const optionGroupSetQuery = useQuery({
+        queryKey: [query],
+        queryFn: queryFn<OptionGroupSetFormValues>,
+    })
+
+    return (
+        <FormBase
+            onSubmit={useOnSubmitNew({ section })}
+            initialValues={omit(optionGroupSetQuery.data, 'id')}
+            validate={validate}
+            fetchError={!!optionGroupSetQuery.error}
+            includeAttributes={false}
+        >
+            <DefaultDuplicateFormContents section={section}>
+                <OptionGroupSetFormFields />
+            </DefaultDuplicateFormContents>
+        </FormBase>
+    )
+}
---
+++ src/pages/optionGroups/Duplicate.tsx
@@ -0,0 +1,45 @@
+import { useQuery } from '@tanstack/react-query'
+import { omit } from 'lodash'
+import React from 'react'
+import { useSearchParams } from 'react-router-dom'
+import { FormBase } from '../../components'
+import { DefaultDuplicateFormContents } from '../../components/form/DefaultFormContents'
+import { SECTIONS_MAP, useOnSubmitNew } from '../../lib'
+import { useBoundResourceQueryFn } from '../../lib/query/useBoundQueryFn'
+import { fieldFilters } from './form/fieldFilters'
+import { OptionGroupFormFields } from './form/OptionGroupFormFields'
+import { OptionGroupFormValues, validate } from './form/OptionGroupFormSchema'
+
+const section = SECTIONS_MAP.optionGroup
+
+export const Component = () => {
+    const queryFn = useBoundResourceQueryFn()
+    const [searchParams] = useSearchParams()
+    const duplicatedModelId = searchParams.get('duplicatedId') as string
+
+    const query = {
+        resource: 'optionGroups',
+        id: duplicatedModelId,
+        params: {
+            fields: fieldFilters.concat(),
+        },
+    }
+    const optionGroupQuery = useQuery({
+        queryKey: [query],
+        queryFn: queryFn<OptionGroupFormValues>,
+    })
+
+    return (
+        <FormBase
+            onSubmit={useOnSubmitNew({ section })}
+            initialValues={omit(optionGroupQuery.data, 'id')}
+            validate={validate}
+            fetchError={!!optionGroupQuery.error}
+            includeAttributes={false}
+        >
+            <DefaultDuplicateFormContents section={section}>
+                <OptionGroupFormFields />
+            </DefaultDuplicateFormContents>
+        </FormBase>
+    )
+}
