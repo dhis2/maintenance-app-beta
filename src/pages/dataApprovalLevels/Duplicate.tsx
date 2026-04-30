5af7e460cd17c30c544f19f1717d4cbefe5cebc8
Thomas Zemp <tom@dhis2.org>
2026-04-30 15:30:24 +0200

    feat: add cloning for data approval items

---
+++ src/pages/dataApprovalLevels/Duplicate.tsx
@@ -0,0 +1,58 @@
+import { useQuery } from '@tanstack/react-query'
+import { omit } from 'lodash'
+import React from 'react'
+import { useSearchParams } from 'react-router-dom'
+import { FormBase } from '../../components'
+import { DefaultDuplicateFormContents } from '../../components/form/DefaultFormContents'
+import { DEFAULT_FIELD_FILTERS, SECTIONS_MAP, useOnSubmitNew } from '../../lib'
+import { useBoundResourceQueryFn } from '../../lib/query/useBoundQueryFn'
+import { PickWithFieldFilters } from '../../types/generated'
+import { DataApprovalLevel } from '../../types/models'
+import DataApprovalLevelFormFields from './form/DataApprovalLevelFormFields'
+import { validate } from './form/dataApprovalLevelsSchema'
+
+const fieldFilters = [
+    ...DEFAULT_FIELD_FILTERS,
+    'name',
+    'orgUnitLevel',
+    'categoryOptionGroupSet[id,displayName]',
+] as const
+
+type DataApprovalLevelFormValues = PickWithFieldFilters<
+    DataApprovalLevel,
+    typeof fieldFilters
+>
+
+const section = SECTIONS_MAP.dataApprovalLevel
+
+export const Component = () => {
+    const queryFn = useBoundResourceQueryFn()
+    const [searchParams] = useSearchParams()
+    const duplicatedModelId = searchParams.get('duplicatedId') as string
+
+    const query = {
+        resource: 'dataApprovalLevels',
+        id: duplicatedModelId,
+        params: {
+            fields: fieldFilters.concat(),
+        },
+    }
+    const dataApprovalLevelQuery = useQuery({
+        queryKey: [query],
+        queryFn: queryFn<DataApprovalLevelFormValues>,
+    })
+
+    return (
+        <FormBase
+            onSubmit={useOnSubmitNew({ section })}
+            initialValues={omit(dataApprovalLevelQuery.data, 'id')}
+            validate={validate}
+            fetchError={!!dataApprovalLevelQuery.error}
+            includeAttributes={false}
+        >
+            <DefaultDuplicateFormContents section={section}>
+                <DataApprovalLevelFormFields />
+            </DefaultDuplicateFormContents>
+        </FormBase>
+    )
+}
---
+++ src/pages/dataApprovalWorkflows/Duplicate.tsx
@@ -0,0 +1,59 @@
+import { useQuery } from '@tanstack/react-query'
+import { omit } from 'lodash'
+import React from 'react'
+import { useSearchParams } from 'react-router-dom'
+import { FormBase } from '../../components'
+import { DefaultDuplicateFormContents } from '../../components/form/DefaultFormContents'
+import { DEFAULT_FIELD_FILTERS, SECTIONS_MAP, useOnSubmitNew } from '../../lib'
+import { useBoundResourceQueryFn } from '../../lib/query/useBoundQueryFn'
+import { PickWithFieldFilters } from '../../types/generated'
+import { DataApprovalWorkflow } from '../../types/models'
+import DataApprovalWorkflowFormFields from './form/DataApprovalWorkflowFormFields'
+import { validate } from './form/dataApprovalWorkflowSchema'
+
+const fieldFilters = [
+    ...DEFAULT_FIELD_FILTERS,
+    'name',
+    'periodType',
+    'categoryCombo[id,displayName]',
+    'dataApprovalLevels[id,displayName]',
+] as const
+
+type DataApprovalWorkflowFormValues = PickWithFieldFilters<
+    DataApprovalWorkflow,
+    typeof fieldFilters
+>
+
+const section = SECTIONS_MAP.dataApprovalWorkflow
+
+export const Component = () => {
+    const queryFn = useBoundResourceQueryFn()
+    const [searchParams] = useSearchParams()
+    const duplicatedModelId = searchParams.get('duplicatedId') as string
+
+    const query = {
+        resource: 'dataApprovalWorkflows',
+        id: duplicatedModelId,
+        params: {
+            fields: fieldFilters.concat(),
+        },
+    }
+    const dataApprovalWorkflowQuery = useQuery({
+        queryKey: [query],
+        queryFn: queryFn<DataApprovalWorkflowFormValues>,
+    })
+
+    return (
+        <FormBase
+            onSubmit={useOnSubmitNew({ section })}
+            initialValues={omit(dataApprovalWorkflowQuery.data, 'id')}
+            validate={validate}
+            fetchError={!!dataApprovalWorkflowQuery.error}
+            includeAttributes={false}
+        >
+            <DefaultDuplicateFormContents section={section}>
+                <DataApprovalWorkflowFormFields />
+            </DefaultDuplicateFormContents>
+        </FormBase>
+    )
+}
